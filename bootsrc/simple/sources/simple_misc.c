c6d645490c618b4507fc1e78d35d020a73293d3c
thecarisma <azeezadewale98@gmail.com>
2018-02-01 16:05:11 +0100

    initialize the boot sources

--- bootsrc/simple/sources/simple_misc.c
+++ bootsrc/simple/sources/simple_misc.c
@@ -21,18 +21,18 @@ RING_API void print_line ( void )
 
 void display_help(){
     printf("Usage:  simple ([OPTIONS]) [PATH] [ARGS...]\n");
-    printf("\t\t\t<to run a simple or complex file>\n");
+    printf("\t\t\t<to run a simple or complex file>\n\n");
     printf("[PATH]     : The Simple/Complex source file to execute.\n");
     printf("[ARG...]   : The arguments to pass to your simple application.\n");
     printf("([OPTIONS]): The flags/options to pass to the Simple VM/Simplify.\n\n");
     printf("The OPTIONS [SIMPLE FILE] include : \n");
-    printf("\t-h -help                    Displays this help and exits.\n");
-    printf("\t-k -show-tokens             Scans and outputs the tokens in the source file.\n");
-    printf("\t-s -simplify                Compile simple file to one complex file.\n");
-    printf("\t-t -time                    Shows time before and after program execution.\n");
-    printf("\t-v -verbose                 Displays all the modules and libraries of SIMPLE and exits.\n");
-    printf("\t-a -about                   Display the SIMPLE Property and exits.\n");
-    printf("\t-z -licence                 Print the current SIMPLE LICENCE and exits.\n");
+    printf("  -h -help                    Displays this help and exits.\n");
+    printf("  -k -show-tokens             Scans and outputs the tokens in the source file.\n");
+    printf("  -s -simplify                Compile simple file to one complex file.\n");
+    printf("  -t -time                    Shows time before and after program execution.\n");
+    printf("  -v -verbose                 Displays all the modules and libraries of SIMPLE and exits.\n");
+    printf("  -a -about                   Display the SIMPLE Property and exits.\n");
+    printf("  -z -licence                 Print the current SIMPLE LICENCE and exits.\n");
     //printf("\t-p -property                Pop up a dialog that shows the current SIMPLE and your Computer Property.\n");
     printf("\t-e -error                   Skip all error instance and print error report in console.\n");
     printf("\t-l [PATH] -log-error [PATH] Skip all error instance and print error report in file.\n");
