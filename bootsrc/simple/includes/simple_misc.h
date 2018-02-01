48333e36437909d1fa9c99f15d280d51362094c8
thecarisma <azeezadewale98@gmail.com>
2018-02-01 15:31:51 +0100

    initialize the boot sources

---
+++ bootsrc/simple/includes/simple_misc.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   simple_misc.h
+ * Author: thecarisma
+ *
+ * Created on February 1, 2018, 3:31 PM
+ */
+
+#ifndef SIMPLE_MISC_H
+#define SIMPLE_MISC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SIMPLE_MISC_H */
+
