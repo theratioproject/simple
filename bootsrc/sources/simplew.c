bedb705ad89f288a0b6c0da6bc11c2691d183776
thecarisma <azeezadewale98@gmail.com>
2018-02-03 00:51:35 +0100

    initializing the bootstrap app

---
+++ bootsrc/sources/simplew.c
@@ -0,0 +1,22 @@
+
+/* 
+	Copyright (c) 2017-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+	MIT License Copyright (c) 2017 simple 
+
+*/
+
+/* 
+ * File:   simple.h
+ * Author: thecarisma
+ *
+ * Created on July 10, 2017, 1:10 PM
+ */
+
+
+#include "../includes/simple.h"
+
+int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
+{
+	simple_state_main(__argc,__argv);
+	return 0 ;
+}
