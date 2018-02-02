677f568e074614e772c639fbceac1c8c318b56ab
thecarisma <azeezadewale98@gmail.com>
2018-02-03 00:08:43 +0100

    stop bootstrapping in c# comparison

---
+++ bootsrc/sources/simple.c
@@ -0,0 +1,21 @@
+
+/* 
+	Copyright (c) 2017-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+	MIT License Copyright (c) 2017 simple 
+
+*/
+
+/* 
+ * File:   simple.c
+ * Author: thecarisma
+ *
+ * Created on July 10, 2017, 1:29 PM
+ */
+
+#include "../includes/simple.h"
+
+int main( int argc, char *argv[])
+{
+	ring_state_main(argc,argv);
+	return 0;
+}
