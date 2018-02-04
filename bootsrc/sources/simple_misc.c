2f635a38bc30ab54f19f9e5621dad6b5cc6fc483
Thecarisma <azeezadewale98@gmail.com>
2018-02-04 10:01:53 +0100

    initializing the bootstrap app

--- bootsrc/sources/simple_misc.c
+++ bootsrc/sources/simple_misc.c
@@ -123,6 +123,9 @@ const char *change_file_ext(const char *absolute_name, const char *extension){
     char* file_name = file_real_name(absolute_name); int name_lenght = strlen(file_name);
     //file_name = (strrchr(absolute_name, '.'))+1;
     for(int a = 0; a<name_lenght;a++) {
+        if (strcmp(file_name[a], ".") == 0){
+            break;
+        }
         printf("%c\n", file_name[a]);
     }
     return file_name;
