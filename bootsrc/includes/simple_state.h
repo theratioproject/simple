f9f99ce6d05f90e384ca99dca3f6c0a2044d6df2
Thecarisma <azeezadewale98@gmail.com>
2018-02-04 20:49:07 +0100

    initializing the bootstrap app

--- bootsrc/includes/simple_state.h
+++ bootsrc/includes/simple_state.h
@@ -99,8 +99,6 @@ int simple_exefilename ( char *cDirPath ) ;
 
 int simple_chdir ( const char *cDir ) ;
 
-void simple_exefolder ( char *cDirPath ) ;
-
 void simple_switchtofilefolder ( char *cFileName ) ;
 
 int simple_justfilepath ( char *cFileName ) ;
