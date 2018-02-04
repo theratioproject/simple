885a582bd2cf837b55c2228e2586860544ad35f8
Thecarisma <azeezadewale98@gmail.com>
2018-02-04 14:26:53 +0100

    initializing the bootstrap app

--- bootsrc/sources/simple_stmt.c
+++ bootsrc/sources/simple_stmt.c
@@ -756,7 +756,7 @@ int simple_parser_stmt ( Parser *parser )
 				break ;
 			}
 		} accept_token_token(parser, OP_BRACECLOSE);
-                printf("\nIN DO..WHILE : %s WHILE : %i\n", parser->TokenText, KEYWORD_WHILE);  simple_parser_nexttoken(parser);
+                printf("\nIN DO..WHILE : %s WHILE : %i\n", parser->TokenText, KEYWORD_WHILE);  
 		if ( simple_parser_iskeyword(parser, KEYWORD_WHILE) ) {
 			/* Generate Code */ 
 			simple_parser_nexttoken(parser);
