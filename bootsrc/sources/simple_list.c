1e1f2f6d4a0d100dbe4060655fe15a11b4925624
Thecarisma <azeezadewale98@gmail.com>
2018-02-05 01:49:31 +0100

    initializing the bootstrap program

--- bootsrc/sources/simple_list.c
+++ bootsrc/sources/simple_list.c
@@ -226,7 +226,7 @@ SIMPLE_API Item * simple_list_getitem ( List *pList,int index )
 		/* Quickly Get The First or The Last Item */
 		if ( index == 0 ) {
 			pList->pLastItemLastAccess = pList->pFirst ;
-			pList->nNextItemAfterLastAccess = index + 1  ;
+			pList->nNextItemAfterLastAccess = index + 2  ;
 			return pList->pFirst->pValue ;
 		}
 		else if ( index == simple_list_getsize(pList) ) {
