393387015ca762b64126bf3688dacb6c750b5df9
Thecarisma <azeezadewale98@gmail.com>
2018-02-06 00:46:00 +0100

    initializing the bootstrap program

--- bootsrc/sources/simple_item.c
+++ bootsrc/sources/simple_item.c
@@ -297,3 +297,44 @@ SIMPLE_API void simple_item_setstring2 ( Item *pItem,const char *cStr,int nStrSi
 {
 	simple_item_setstring2_gc(NULL,pItem,cStr,nStrSize);
 }
+
+Items * simple_items_new_gc ( void *pState )
+{
+	Items *pItems  ;
+	pItems = (Items *) simple_state_malloc(pState,sizeof(Items));
+	if ( pItems == NULL ) {
+		printf( SIMPLE_OOM ) ;
+		exit(0);
+	}
+	pItems->pValue = simple_item_new_gc(pState,ITEMTYPE_NOTHING) ;
+	pItems->pNext = NULL ;
+	pItems->pPrev = NULL ;
+	return pItems ;
+}
+
+Items * simple_items_delete_gc ( void *pState,Items *pItems )
+{
+	assert(pItems != NULL);
+	pItems->pValue = simple_item_delete_gc(pState,pItems->pValue);
+	simple_state_free(pState,pItems);
+	pItems = NULL ;
+	return pItems ;
+}
+
+void simple_items_print ( Items *pItems )
+{
+	assert(pItems != NULL);
+	simple_item_print(pItems->pValue);
+}
+/* Functions without State pointer */
+
+Items * simple_items_new ( void )
+{
+	return simple_items_new_gc(NULL) ;
+}
+
+Items * simple_items_delete ( Items *pItems )
+{
+	return simple_items_delete_gc(NULL,pItems) ;
+}
+
