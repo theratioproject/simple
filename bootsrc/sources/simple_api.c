8cb296fba5465b41d709d964d1d66a016986d6a3
Thecarisma <azeezadewale98@gmail.com>
2018-02-23 10:46:43 +0100

    modify the bootstrap program

--- bootsrc/sources/simple_api.c
+++ bootsrc/sources/simple_api.c
@@ -461,7 +461,7 @@ SIMPLE_API void simple_vmlib_exec ( void *pointer )
 	if ( SIMPLE_API_ISSTRING(1) ) {
 		cStr = SIMPLE_API_GETSTRING(1);
 		vm = (VM *) pointer ;
-		vm->nEvalCalledFromSimpleCode = 1 ;
+		//vm->nEvalCalledFromSimpleCode = 1 ;
 		if ( simple_vm_exec(vm,cStr) == 0 ) {
 			vm->nEvalCalledFromSimpleCode = 0 ;
 		}
