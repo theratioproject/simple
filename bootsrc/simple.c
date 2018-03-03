db85a42a686e98a3f935bd496e9fc6dc83916365
Ikechi Michael <mykehell123@gmail.com>
2018-03-03 15:12:20 +0100

    feat: add file to build executable

---
+++ bootsrc/simple.c
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   simple.c
+ * Author: thecarisma
+ *
+ * Created on February 12, 2018, 11:46 PM
+ */
+
+ #include "./includes/simple.h"
+ 
+ /*
+  * 
+  */
+ int main(int argc, char** argv) {
+     
+     simple_state_main(argc,argv);
+     return 0;
+ }
