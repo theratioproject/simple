f80051308268123c785fca982cca9d25f3c51865
Thecarisma <azeezadewale98@gmail.com>
2018-03-06 16:13:03 +0100

    add properties to readme

--- bootsrc/includes/simple.h
+++ bootsrc/includes/simple.h
@@ -69,6 +69,7 @@
 /* See and Give - use simplevm_display() and read_string() */
 #define SIMPLE_USEDISPLAYBLOCKTION 1
 #define SIMPLE_USEGIVEBLOCKTION 1
+#define true 1
 /*
 **  Include Files 
 **  Include C Headers 
