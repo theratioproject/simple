346df856f91a65fd46355a9113a7226737293ae0
Thecarisma <azeezadewale98@gmail.com>
2018-04-10 20:52:15 +0100

    create universal Makefile to generate debug

--- src/Makefile-Windows.mk
+++ src/Makefile-Windows.mk
@@ -90,104 +90,10 @@ ${CND_DISTDIR}/${CND_PLATFORM}/simple.${CND_DLIB_EXT}: ${OBJECTFILES}
 	${RMDIR} ${OBJECTDIR}/sources
 	${RMDIR} ${OBJECTDIR}
 
-${OBJECTDIR}/sources/simple_api.o: sources/simple_api.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_api.o sources/simple_api.c
-
-${OBJECTDIR}/sources/simple_codegen.o: sources/simple_codegen.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_codegen.o sources/simple_codegen.c
-
-${OBJECTDIR}/sources/simple_expr.o: sources/simple_expr.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_expr.o sources/simple_expr.c
-
-${OBJECTDIR}/sources/simple_hashtable.o: sources/simple_hashtable.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_hashtable.o sources/simple_hashtable.c
-
-${OBJECTDIR}/sources/simple_item.o: sources/simple_item.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_item.o sources/simple_item.c
-
-${OBJECTDIR}/sources/simple_list.o: sources/simple_list.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_list.o sources/simple_list.c
-
-${OBJECTDIR}/sources/simple_misc.o: sources/simple_misc.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_misc.o sources/simple_misc.c
-
-${OBJECTDIR}/sources/simple_parser.o: sources/simple_parser.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_parser.o sources/simple_parser.c
-
-${OBJECTDIR}/sources/simple_scanner.o: sources/simple_scanner.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_scanner.o sources/simple_scanner.c
-
-${OBJECTDIR}/sources/simple_state.o: sources/simple_state.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_state.o sources/simple_state.c
-
-${OBJECTDIR}/sources/simple_stmt.o: sources/simple_stmt.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_stmt.o sources/simple_stmt.c
-
-${OBJECTDIR}/sources/simple_string.o: sources/simple_string.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_string.o sources/simple_string.c
-
-${OBJECTDIR}/sources/simple_vm.o: sources/simple_vm.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vm.o sources/simple_vm.c
-
-${OBJECTDIR}/sources/simple_vmblock.o: sources/simple_vmblock.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmblock.o sources/simple_vmblock.c
-
-${OBJECTDIR}/sources/simple_vmdll.o: sources/simple_vmdll.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmdll.o sources/simple_vmdll.c
-
-${OBJECTDIR}/sources/simple_vmexpr.o: sources/simple_vmexpr.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmexpr.o sources/simple_vmexpr.c
-
-${OBJECTDIR}/sources/simple_vmgc.o: sources/simple_vmgc.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmgc.o sources/simple_vmgc.c
-
-${OBJECTDIR}/sources/simple_vmlists.o: sources/simple_vmlists.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmlists.o sources/simple_vmlists.c
-
-${OBJECTDIR}/sources/simple_vmoop.o: sources/simple_vmoop.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmoop.o sources/simple_vmoop.c
-
-${OBJECTDIR}/sources/simple_vmperformance.o: sources/simple_vmperformance.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmperformance.o sources/simple_vmperformance.c
-
-${OBJECTDIR}/sources/simple_vmstackvars.o: sources/simple_vmstackvars.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmstackvars.o sources/simple_vmstackvars.c
-
-${OBJECTDIR}/sources/simple_vmstate.o: sources/simple_vmstate.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmstate.o sources/simple_vmstate.c
-
-${OBJECTDIR}/sources/simple_vmstrindex.o: sources/simple_vmstrindex.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmstrindex.o sources/simple_vmstrindex.c
-
-${OBJECTDIR}/sources/simple_vmvars.o: sources/simple_vmvars.c
-	${MKDIR} -p ${OBJECTDIR}/sources
-	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/simple_vmvars.o sources/simple_vmvars.c
+${OBJECTDIR}/sources/: sources/*.c
+	$(COMPILE.c) -g  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/sources/ sources/*.c
 	
 ${OBJECTDIR}/simple.o: simple.c
-	${MKDIR} -p ${OBJECTDIR}
 	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/simple.o simple.c
 
 
