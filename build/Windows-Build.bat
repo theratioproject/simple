4deb9c5b7b9e2cb4eaffc0a9426182d6cda52721
Thecarisma <azeezadewale98@gmail.com>
2018-04-10 21:47:15 +0100

    create universal Makefile to generate debug

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -1,12 +1,14 @@
 echo off
 cls
 
-if exist "../src/Makefile-Windows.mk" (
+if exist "../src/Makefile-Windows.mks" (
 	cd "../src"
 	echo `
 	echo `
 	echo 		SIMPLE s0.3.302-debug build 
 	echo		Building simple.dll and simple.exe
+	make -f Makefile-Windows.mk
+	cd ../build
 	exit /b
 ) else (
 	echo `
@@ -15,3 +17,5 @@ if exist "../src/Makefile-Windows.mk" (
 	echo 		The file 'Makefile-Windows.mk' does not exist in src folder
 	echo 		Stopping Build
 )
+
+~Build has been successful
