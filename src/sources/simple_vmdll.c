8e0904c8a1ecd0725241e8a8976c859666e10a37
Thecarisma <azeezadewale98@gmail.com>
2018-04-11 19:17:03 +0100

    resolve simple to use immediate modules in same distro before looking in other location

--- src/sources/simple_vmdll.c
+++ src/sources/simple_vmdll.c
@@ -41,10 +41,13 @@ void simple_vm_dll_loadlib ( void *pointer )
         if (simple_fexists(SIMPLE_API_GETSTRING(1))) {
             strcpy(library_path,SIMPLE_API_GETSTRING(1));
         } else {
-            char* simple_folder ; simple_distro_folder(simple_folder); printf("We found it %s\n",simple_folder);
-            snprintf(library_path, sizeof(library_path), "%s/modules/dynamic_modules/%s", simple_folder, SIMPLE_VERSION, SIMPLE_API_GETSTRING(1));
-            if ()
-            char* SIMPLEPATH = getenv("SIMPLE_PATH"); 
+            char* simple_folder ; simple_distro_folder(simple_folder); 
+            snprintf(library_path, sizeof(library_path), "%s\\modules\\dynamic_modules\\%s", simple_folder,SIMPLE_API_GETSTRING(1));
+            printf("We found it %s\n",library_path);
+            if (!simple_fexists(library_path)) {
+                
+            }
+            /**char* SIMPLEPATH = getenv("SIMPLE_PATH"); 
             if (SIMPLEPATH != NULL) {
                 snprintf(library_path, sizeof(library_path), "%s/s%s/modules/dynamic_modules/%s", SIMPLEPATH, SIMPLE_VERSION, SIMPLE_API_GETSTRING(1));
             }
@@ -72,7 +75,7 @@ void simple_vm_dll_loadlib ( void *pointer )
 
                     }
                 }
-            } 
+            }  **/
         }
         cDLL = library_path;
         handle = LoadDLL(cDLL);
