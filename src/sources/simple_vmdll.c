b7f16df539210b8a0cc3f620548ad5ee1fb5fce4
Thecarisma <azeezadewale98@gmail.com>
2018-04-11 19:02:02 +0100

    resolve simple to use immediate modules in same distro before looking in other location

--- src/sources/simple_vmdll.c
+++ src/sources/simple_vmdll.c
@@ -41,7 +41,8 @@ void simple_vm_dll_loadlib ( void *pointer )
         if (simple_fexists(SIMPLE_API_GETSTRING(1))) {
             strcpy(library_path,SIMPLE_API_GETSTRING(1));
         } else {
-            char* test ; simple_exefolder(test); printf("We found it %s\n",test);
+            char* simple_folder ; simple_exefolder(simple_folder); simple_justfilepath(simple_folder); printf("We found it %s\n",simple_folder);
+            //if ()
             char* SIMPLEPATH = getenv("SIMPLE_PATH"); 
             if (SIMPLEPATH != NULL) {
                 snprintf(library_path, sizeof(library_path), "%s/s%s/modules/dynamic_modules/%s", SIMPLEPATH, SIMPLE_VERSION, SIMPLE_API_GETSTRING(1));
