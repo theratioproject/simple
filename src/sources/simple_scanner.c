4bf735cc353f195b6ebba5afa476e9aa3148caa0
Thecarisma <azeezadewale98@gmail.com>
2018-04-12 01:09:50 +0100

    resolve simple to use immediate modules in same distro before looking in other location

--- src/sources/simple_scanner.c
+++ src/sources/simple_scanner.c
@@ -112,12 +112,14 @@ int simple_scanner_readfile ( SimpleState *sState,char *file_name )
                                 get_file_folder(simple_folder); 
                                 snprintf(file_name_two, sizeof(file_name_two), "%s/s%s/modules/%s", simple_folder, SIMPLE_VERSION, file_name);
                                 if (!simple_fexists(file_name_two)) {
-                                    #if SIMPLE_DEVELOPMENT
-                                        snprintf(file_name_two, sizeof(file_name_two), "%s/%s", "C:/Users/thecarisma/Documents/SIMPLE GITHUB/modules", file_name);
-                                    #else
-                                        //deduct_char_from_behind(simple_folder, 4); //to remove bin
-                                        snprintf(file_name_two, sizeof(file_name_two), "%s/s%s/modules/%s", simple_folder, SIMPLE_VERSION, file_name);
-                                        printf("NOW CHECKING AGAIN %s \n",file_name_two);
+                                    #if SIMPLE_DEVELOPMENT 
+										#ifdef _WIN32
+											snprintf(file_name_two, sizeof(file_name_two), "%s/%s", "C:/Users/thecarisma/Documents/SIMPLE GITHUB/modules", file_name);
+										#else
+											//deduct_char_from_behind(simple_folder, 4); //to remove bin
+											snprintf(file_name_two, sizeof(file_name_two), "%s/s%s/modules/%s", simple_folder, SIMPLE_VERSION, file_name);
+											printf("NOW CHECKING AGAIN %s \n",file_name_two);
+										#endif
                                     #endif
                                 }
                             }
