421bd53949a0927653ebfc5554a469deba3064d4
Azeez Adewale <azeezadewale98@gmail.com>
2018-04-14 16:38:51 +0000

    cure the module curse and heal the module fleet

--- build/BuildEnvironment.bat
+++ build/BuildEnvironment.bat
@@ -3,6 +3,9 @@ echo off
 	echo `
 	echo 	Building Environment Program to Executable 
 	
+SET SMAKE=""
+ECHO We're working with %SMAKE%
+	
 REM Confirm smake is present
 if exist "../../s0.3.302-debug/environment/smake.sim" (
 	echo 		Smake Found. yea Starting Build...
@@ -26,4 +29,19 @@ if exist "../../s0.3.302-debug/environment/SimplePad.sim" (
 ) else (
 	echo 		../../s0.3.302-debug/environment/SimplePad.sim cannot be found
 	echo 		skipping SimplePad
+)
+
+REM Builing REPL
+echo 		`SimpleRepl
+if exist "../../s0.3.302-debug/environment/SimpleRepl.sim" (
+	if exist ../../simple-arts/environment/simplerepl.ico (
+		echo 		Building SimpleRepl with icon
+		simple ../../s0.3.302-debug/environment/smake.sim -I/../../simple-arts/environment/simpleprepl.ico -delete ../../s0.3.302-debug/environment/SimpleRepl.sim	
+	) else (
+		echo 		Building SimpleRepl
+		simple ../../s0.3.302-debug/environment/smake.sim -delete ../../s0.3.302-debug/environment/SimpleRepl.sim
+	)
+) else (
+	echo 		../../s0.3.302-debug/environment/SimpleRepl.sim cannot be found
+	echo 		skipping SimpleRepl
 )
