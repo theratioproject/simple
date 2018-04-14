bd63c65fed3c04a058d8a8bb7148c32d886e37f8
Azeez Adewale <azeezadewale98@gmail.com>
2018-04-14 16:23:50 +0000

    cure the module curse and heal the module fleet

--- build/BuildEnvironment.bat
+++ build/BuildEnvironment.bat
@@ -1,19 +1,25 @@
-REM Confirm smake is present
-if exist "../../s0.3.302-debug/environment/simplepad/SimplePad.sim" (
-	echo 		Building SimplePad
+echo off
+
+	echo `
+	echo 	Building Environment Program to Executable 
 	
+REM Confirm smake is present
+if exist "../../s0.3.302-debug/environment/smake.sim" (
+	echo 		Smake Found. yea Starting Build..
+	simple smake.sim -I/window.ico ../../backup/smake/SimpleRepl.sim
 ) else (
 	echo 		smake.sim cannot be found
 	echo 		no smake no build bye
+	REM exit 
 )
 
 
 REM Builing SimplePad
 echo 		`SimplePad
-if exist "../../s0.3.302-debug/environment/simplepad/SimplePad.sim" (
+if exist "../../s0.3.302-debug/environment/SimplePad.sim" (
 	echo 		Building SimplePad
 	
 ) else (
-	echo 		../../s0.3.302-debug/environment/simplepad/SimplePad.sim cannot be found
+	echo 		../../s0.3.302-debug/environment/SimplePad.sim cannot be found
 	echo 		skipping SimplePad
 )
