6c268f7b77a080cf193ec531ab054b96194194a0
Azeez Adewale <azeezadewale98@gmail.com>
2018-04-14 16:53:51 +0000

    cure the module curse and heal the module fleet

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -1,40 +1,42 @@
 echo off
 cls
 
+SET SIMPLE_DEBUG_VERSION="s0.3.302-debug"
+
 REM BULDING SIMPLE.EXE and SIMPLE.DLL
 if exist "../src/makefiles/Makefile-Windows.mk" (
 	cd "../src/makefiles"
 	echo `
 	echo `
-	echo 	SIMPLE s0.3.302-debug build 
+	echo 	SIMPLE %SIMPLE_DEBUG_VERSION% build 
 	echo		Building simple.dll and simple.exe
 	make -f Makefile-Windows.mk
 	cd ../../build
 ) else (
 	echo `
 	echo `
-	echo 	SIMPLE s0.3.302-debug build 
+	echo 	SIMPLE %SIMPLE_DEBUG_VERSION% build 
 	echo 		The file 'Makefile-Windows.mk' does not exist in src folder
 	echo 		Skipping simple Build
 )
 
-REM simple.exe and simple.dll has been successful create and copy executable to s0.3.302-debug folder
+REM simple.exe and simple.dll has been successful create and copy executable to %SIMPLE_DEBUG_VERSION% folder
 	echo `
-	echo 	Copying Executable and building s0.3.302-debug
+	echo 	Copying Executable and building %SIMPLE_DEBUG_VERSION%
 
 
-if exist "../../s0.3.302-debug/bin/" (
-	echo 		the ../../s0.3.302-debug/bin/ folder already exist
+if exist "../../%SIMPLE_DEBUG_VERSION%/bin/" (
+	echo 		the ../../%SIMPLE_DEBUG_VERSION%/bin/ folder already exist
 ) else (
-	echo 		Creating the ../../s0.3.302-debug/bin/ folder
-	mkdir "../../s0.3.302-debug/bin/"
+	echo 		Creating the ../../%SIMPLE_DEBUG_VERSION%/bin/ folder
+	mkdir "../../%SIMPLE_DEBUG_VERSION%/bin/"
 )
 
 if exist "../src/dist/simple.exe" (
-	echo 		Copying simple.exe and simple.dll to ../../s0.3.302-debug/bin folder
-	cp ../src/dist/simple.exe ../../s0.3.302-debug/bin
-	cp ../src/dist/simplew.exe ../../s0.3.302-debug/bin
-	cp ../src/dist/simple.dll ../../s0.3.302-debug/bin
+	echo 		Copying simple.exe and simple.dll to ../../%SIMPLE_DEBUG_VERSION%/bin folder
+	cp ../src/dist/simple.exe ../../%SIMPLE_DEBUG_VERSION%/bin
+	cp ../src/dist/simplew.exe ../../%SIMPLE_DEBUG_VERSION%/bin
+	cp ../src/dist/simple.dll ../../%SIMPLE_DEBUG_VERSION%/bin
 ) else (
 	echo 		Build fails simple.exe and simple.dll cannot be found
 	echo 		in the ../src/dist/ folder try rebuilding again if it
@@ -66,20 +68,20 @@ if exist "../../modules" (
 	echo 		if ignored only the simple.exe and simple.dll will be built
 )
 
-REM the dynamic libraries has been successful generated now copy them to s0.3.302-debug folder
+REM the dynamic libraries has been successful generated now copy them to %SIMPLE_DEBUG_VERSION% folder
 	echo `
-	echo 	Copying Dynamic Modules to s0.3.302-debug
+	echo 	Copying Dynamic Modules to %SIMPLE_DEBUG_VERSION%
 
-if exist "../../s0.3.302-debug/modules/dynamic_modules" (
-	echo 		the ../../s0.3.302-debug/modules/dynamic_modules folder already exist
+if exist "../../%SIMPLE_DEBUG_VERSION%/modules/dynamic_modules" (
+	echo 		the ../../%SIMPLE_DEBUG_VERSION%/modules/dynamic_modules folder already exist
 ) else (
-	echo 		Creating the ../../s0.3.302-debug/modules/dynamic_modules folder
-	mkdir "../../s0.3.302-debug/modules/dynamic_modules"
+	echo 		Creating the ../../%SIMPLE_DEBUG_VERSION%/modules/dynamic_modules folder
+	mkdir "../../%SIMPLE_DEBUG_VERSION%/modules/dynamic_modules"
 )
 
 if exist "../../modules/dynamic_modules/dist/systemic.dll" (
-	echo 		Copying dynamic modules to ../../s0.3.302-debug/modules/dynamic_modules folder
-	cp ../../modules/dynamic_modules/dist/*.dll ../../s0.3.302-debug/modules/dynamic_modules
+	echo 		Copying dynamic modules to ../../%SIMPLE_DEBUG_VERSION%/modules/dynamic_modules folder
+	cp ../../modules/dynamic_modules/dist/*.dll ../../%SIMPLE_DEBUG_VERSION%/modules/dynamic_modules
 ) else (
 	echo 		Build fails the dynamic modules cannot be found
 	echo 		in the ../../modules/dynamic_modules/dist folder try 
@@ -90,8 +92,8 @@ REM fulltick(GUI) dynamic module
 	echo `
 	echo 	Building of fulltick(GUI) modules not currently supported
 if exist "../../modules/dynamic_modules/fulltick/dist/fulltick.dll" (
-	echo 		Copying fulltick.dll to ../../s0.3.302-debug/bin folder
-	cp ../../modules/dynamic_modules/fulltick/dist/fulltick.dll ../../s0.3.302-debug/modules/dynamic_modules
+	echo 		Copying fulltick.dll to ../../%SIMPLE_DEBUG_VERSION%/bin folder
+	cp ../../modules/dynamic_modules/fulltick/dist/fulltick.dll ../../%SIMPLE_DEBUG_VERSION%/modules/dynamic_modules
 ) else (
 	echo 		The fulltick.dll built module cannot be found 
 	echo 		Skipping the GUI module build
@@ -104,45 +106,45 @@ REM resolve dependencies
 REM ssleay32.dll
 	echo 		`ssleay32.dll
 if exist "../../modules/dynamic_modules/security/bin/ssleay32.dll" (
-	echo 		Copying ssleay32.dll to ../../s0.3.302-debug/bin folder
-	cp ../../modules/dynamic_modules/security/bin/ssleay32.dll ../../s0.3.302-debug/bin
+	echo 		Copying ssleay32.dll to ../../%SIMPLE_DEBUG_VERSION%/bin folder
+	cp ../../modules/dynamic_modules/security/bin/ssleay32.dll ../../%SIMPLE_DEBUG_VERSION%/bin
 ) else (
 	echo 		The dependency ssleay32.dll cannot be found you can copy it 
-	echo 		in to the ../../s0.3.302-debug/bin folder manually if it is 
+	echo 		in to the ../../%SIMPLE_DEBUG_VERSION%/bin folder manually if it is 
 	echo 		not included in your cloned repository. Download it from reliable source
 )
 
 REM libeay32.dll
 	echo 		`libeay32.dll
 if exist "../../modules/dynamic_modules/security/bin/libeay32.dll" (
-	echo 		Copying libeay32.dll to ../../s0.3.302-debug/bin folder
-	cp ../../modules/dynamic_modules/security/bin/libeay32.dll ../../s0.3.302-debug/bin
+	echo 		Copying libeay32.dll to ../../%SIMPLE_DEBUG_VERSION%/bin folder
+	cp ../../modules/dynamic_modules/security/bin/libeay32.dll ../../%SIMPLE_DEBUG_VERSION%/bin
 ) else (
 	echo 		The dependency libeay32.dll cannot be found you can copy it 
-	echo 		in to the ../../s0.3.302-debug/bin folder manually if it is 
+	echo 		in to the ../../%SIMPLE_DEBUG_VERSION%/bin folder manually if it is 
 	echo 		not included in your cloned repository. Download it from reliable source
 )
 
 REM libcurl.dll
 	echo 		`libcurl.dll
 if exist "../../modules/dynamic_modules/networker/lib/libcurl.dll" (
-	echo 		Copying libcurl.dll to ../../s0.3.302-debug/bin folder
-	cp ../../modules/dynamic_modules/networker/lib/libcurl.dll ../../s0.3.302-debug/bin
+	echo 		Copying libcurl.dll to ../../%SIMPLE_DEBUG_VERSION%/bin folder
+	cp ../../modules/dynamic_modules/networker/lib/libcurl.dll ../../%SIMPLE_DEBUG_VERSION%/bin
 ) else (
 	echo 		The dependency libcurl.dll cannot be found you can copy it 
-	echo 		in to the ../../s0.3.302-debug/bin folder manually if it is 
+	echo 		in to the ../../%SIMPLE_DEBUG_VERSION%/bin folder manually if it is 
 	echo 		not included in your cloned repository. Download it from reliable source
 )
 
 REM Copy the simple modules
 	echo `
-	echo 	Copying Simple Modules to s0.3.302-debug
+	echo 	Copying Simple Modules to %SIMPLE_DEBUG_VERSION%
 
 REM simple core module	
 	echo 		`simple core module
 if exist "../../modules/simple" (
-	echo 		Copying simple module to ../../s0.3.302-debug/modules folder
-	xcopy "../../modules/simple" "../../s0.3.302-debug/modules/simple/" /s /h /e /k /f /c
+	echo 		Copying simple module to ../../%SIMPLE_DEBUG_VERSION%/modules folder
+	xcopy "../../modules/simple" "../../%SIMPLE_DEBUG_VERSION%/modules/simple/" /s /h /e /k /f /c
 ) else (
 	echo 		simple module cannot be found
 	echo 		The repository appears to be currupted. Try clonning
@@ -152,8 +154,8 @@ if exist "../../modules/simple" (
 REM archive module	
 	echo 		`archive module
 if exist "../../modules/archive" (
-	echo 		Copying archive module to ../../s0.3.302-debug/modules folder
-	xcopy "../../modules/archive" "../../s0.3.302-debug/modules/archive/" /s /h /e /i /k /f /c
+	echo 		Copying archive module to ../../%SIMPLE_DEBUG_VERSION%/modules folder
+	xcopy "../../modules/archive" "../../%SIMPLE_DEBUG_VERSION%/modules/archive/" /s /h /e /i /k /f /c
 ) else (
 	echo 		archive module cannot be found
 	echo 		The repository appears to be currupted. Try clonning
@@ -163,8 +165,8 @@ if exist "../../modules/archive" (
 REM web module	
 	echo 		`web module
 if exist "../../modules/web" (
-	echo 		Copying web module to ../../s0.3.302-debug/modules folder
-	xcopy "../../modules/web" "../../s0.3.302-debug/modules/web/" /s /h /e /i /k /f /c
+	echo 		Copying web module to ../../%SIMPLE_DEBUG_VERSION%/modules folder
+	xcopy "../../modules/web" "../../%SIMPLE_DEBUG_VERSION%/modules/web/" /s /h /e /i /k /f /c
 ) else (
 	echo 		web module cannot be found
 	echo 		The repository appears to be currupted. Try clonning
@@ -174,8 +176,8 @@ if exist "../../modules/web" (
 REM fulltick(GUI) module	
 	echo 		`fulltick(GUI) module
 if exist "../../modules/fulltick" (
-	echo 		Copying fulltick module to ../../s0.3.302-debug/modules folder
-	xcopy "../../modules/fulltick" "../../s0.3.302-debug/modules/fulltick/" /s /h /e /i /k /f /c
+	echo 		Copying fulltick module to ../../%SIMPLE_DEBUG_VERSION%/modules folder
+	xcopy "../../modules/fulltick" "../../%SIMPLE_DEBUG_VERSION%/modules/fulltick/" /s /h /e /i /k /f /c
 ) else (
 	echo 		fulltick module cannot be found
 	echo 		The repository appears to be currupted. Try clonning
@@ -183,22 +185,22 @@ if exist "../../modules/fulltick" (
 )
 
 REM ENVIRONMENT PROGRAMS
-REM move the environment to s0.3.302-debug folder
+REM move the environment to %SIMPLE_DEBUG_VERSION% folder
 	echo `
-	echo 	Copying Environment Programs to s0.3.302-debug
+	echo 	Copying Environment Programs to %SIMPLE_DEBUG_VERSION%
 	
-if exist "../../s0.3.302-debug/environment" (
-	echo 		the ../../s0.3.302-debug/environment folder already exist
+if exist "../../%SIMPLE_DEBUG_VERSION%/environment" (
+	echo 		the ../../%SIMPLE_DEBUG_VERSION%/environment folder already exist
 ) else (
-	echo 		Creating the ../../s0.3.302-debug/environment folder
-	mkdir "../../s0.3.302-debug/environment"
+	echo 		Creating the ../../%SIMPLE_DEBUG_VERSION%/environment folder
+	mkdir "../../%SIMPLE_DEBUG_VERSION%/environment"
 )
 
 REM modular	
 	echo 		`modular
 if exist "../../environment/modular/modular.sim" (
-	echo 		Copying modular to ../../s0.3.302-debug/environment folder
-	cp ../../environment/modular/modular.sim ../../s0.3.302-debug/environment
+	echo 		Copying modular to ../../%SIMPLE_DEBUG_VERSION%/environment folder
+	cp ../../environment/modular/modular.sim ../../%SIMPLE_DEBUG_VERSION%/environment
 ) else (
 	echo 		../../environment/modular/modular.sim cannot be found
 	echo 		skipping modular
@@ -207,8 +209,8 @@ if exist "../../environment/modular/modular.sim" (
 REM repl
 	echo 		`repl
 if exist "../../environment/repl/SimpleRepl.sim" (
-	echo 		Copying repl to ../../s0.3.302-debug/environment folder
-	cp ../../environment/repl/SimpleRepl.sim ../../s0.3.302-debug/environment
+	echo 		Copying repl to ../../%SIMPLE_DEBUG_VERSION%/environment folder
+	cp ../../environment/repl/SimpleRepl.sim ../../%SIMPLE_DEBUG_VERSION%/environment
 ) else (
 	echo 		../../environment/repl/SimpleRepl.sim cannot be found
 	echo 		skipping repl
@@ -217,10 +219,10 @@ if exist "../../environment/repl/SimpleRepl.sim" (
 REM simplepad
 	echo 		`simplepad
 if exist "../../environment/simplepad/SimplePad.sim" (
-	echo 		Copying simplepad to ../../s0.3.302-debug/environment folder
-	cp ../../environment/simplepad/SimplePad.sim ../../s0.3.302-debug/environment
-	REM cp ../../environment/simplepad/runwindows.bat ../../s0.3.302-debug/environment
-	REM cp ../../environment/simplepad/debugwindows.bat ../../s0.3.302-debug/environment
+	echo 		Copying simplepad to ../../%SIMPLE_DEBUG_VERSION%/environment folder
+	cp ../../environment/simplepad/SimplePad.sim ../../%SIMPLE_DEBUG_VERSION%/environment
+	REM cp ../../environment/simplepad/runwindows.bat ../../%SIMPLE_DEBUG_VERSION%/environment
+	REM cp ../../environment/simplepad/debugwindows.bat ../../%SIMPLE_DEBUG_VERSION%/environment
 ) else (
 	echo 		../../environment/simplepad/SimplePad.sim cannot be found
 	echo 		skipping simplepad
@@ -229,8 +231,8 @@ if exist "../../environment/simplepad/SimplePad.sim" (
 REM smake 
 echo 		`smake
 if exist "../../environment/smake/smake.sim" (
-	echo 		Copying smake to ../../s0.3.302-debug/environment folder
-	cp ../../environment/smake/smake.sim ../../s0.3.302-debug/environment
+	echo 		Copying smake to ../../%SIMPLE_DEBUG_VERSION%/environment folder
+	cp ../../environment/smake/smake.sim ../../%SIMPLE_DEBUG_VERSION%/environment
 ) else (
 	echo 		../../environment/smake/SimplePad.sim cannot be found
 	echo 		skipping smake
@@ -239,8 +241,8 @@ if exist "../../environment/smake/smake.sim" (
 REM spider
 	echo 		`spider
 if exist "../../environment/spider/spider.sim" (
-	echo 		Copying spider to ../../s0.3.302-debug/environment folder
-	cp ../../environment/spider/spider.sim ../../s0.3.302-debug/environment
+	echo 		Copying spider to ../../%SIMPLE_DEBUG_VERSION%/environment folder
+	cp ../../environment/spider/spider.sim ../../%SIMPLE_DEBUG_VERSION%/environment
 ) else (
 	echo 		../../environment/spider/SimplePad.sim cannot be found
 	echo 		skipping spider
@@ -254,12 +256,13 @@ echo off
 	echo 	Building Environment Program to Executable 
 	
 SET SMAKE=""
+SET SIMPLE=""
 
 REM Confirm simple is present
 
-if exist "../../s0.3.302-debug/bin/simple.exe" (
-	echo 		simple Found. yea now chek for smake...
-	SET SIMPLE="../../s0.3.302-debug/bin/simple.exe"
+if exist "../../%SIMPLE_DEBUG_VERSION%/bin/simple.exe" (
+	echo 		simple Found. yea now to check for smake...
+	SET SIMPLE="../../%SIMPLE_DEBUG_VERSION%/bin/simple.exe"
 ) else (
 	echo 		simple cannot be found
 	echo 		The build process failed bye
@@ -268,9 +271,9 @@ if exist "../../s0.3.302-debug/bin/simple.exe" (
 	
 REM Confirm smake is present
 
-if exist "../../s0.3.302-debug/environment/smake.sim" (
+if exist "../../%SIMPLE_DEBUG_VERSION%/environment/smake.sim" (
 	echo 		Smake Found. yea Starting Build...
-	SET SMAKE="../../s0.3.302-debug/environment/smake.sim"
+	SET SMAKE="../../%SIMPLE_DEBUG_VERSION%/environment/smake.sim"
 ) else (
 	echo 		smake.sim cannot be found
 	echo 		no smake no build bye
@@ -279,31 +282,31 @@ if exist "../../s0.3.302-debug/environment/smake.sim" (
 
 REM Builing SimplePad
 echo 		`SimplePad
-if exist "../../s0.3.302-debug/environment/SimplePad.sim" (
+if exist "../../%SIMPLE_DEBUG_VERSION%/environment/SimplePad.sim" (
 	if exist ../../simple-arts/environment/simplepad.ico (
 		echo 		Building SimplePad with icon
-		%SIMPLE% %SMAKE% -I/../../simple-arts/environment/simplepad.ico -gui -delete ../../s0.3.302-debug/environment/SimplePad.sim	
+		%SIMPLE% %SMAKE% -I/../../simple-arts/environment/simplepad.ico -gui -delete ../../%SIMPLE_DEBUG_VERSION%/environment/SimplePad.sim	
 	) else (
 		echo 		Building SimplePad
-		%SIMPLE% %SMAKE% -delete ../../s0.3.302-debug/environment/SimplePad.sim
+		%SIMPLE% %SMAKE% -delete ../../%SIMPLE_DEBUG_VERSION%/environment/SimplePad.sim
 	)
 ) else (
-	echo 		../../s0.3.302-debug/environment/SimplePad.sim cannot be found
+	echo 		../../%SIMPLE_DEBUG_VERSION%/environment/SimplePad.sim cannot be found
 	echo 		skipping SimplePad
 )
 
 REM Builing REPL
 echo 		`SimpleRepl
-if exist "../../s0.3.302-debug/environment/SimpleRepl.sim" (
+if exist "../../%SIMPLE_DEBUG_VERSION%/environment/SimpleRepl.sim" (
 	if exist ../../simple-arts/environment/simplerepl.ico (
 		echo 		Building SimpleRepl with icon
-		%SIMPLE% %SMAKE% -I/../../simple-arts/environment/simpleprepl.ico -delete ../../s0.3.302-debug/environment/SimpleRepl.sim	
+		%SIMPLE% %SMAKE% -I/../../simple-arts/environment/simpleprepl.ico -delete ../../%SIMPLE_DEBUG_VERSION%/environment/SimpleRepl.sim	
 	) else (
 		echo 		Building SimpleRepl
-		%SIMPLE% %SMAKE% -delete ../../s0.3.302-debug/environment/SimpleRepl.sim
+		%SIMPLE% %SMAKE% -delete ../../%SIMPLE_DEBUG_VERSION%/environment/SimpleRepl.sim
 	)
 ) else (
-	echo 		../../s0.3.302-debug/environment/SimpleRepl.sim cannot be found
+	echo 		../../%SIMPLE_DEBUG_VERSION%/environment/SimpleRepl.sim cannot be found
 	echo 		skipping SimpleRepl
 )
 
