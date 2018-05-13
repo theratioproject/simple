6064fbe8bc6194c4a965c13649bc0e223c1afe3f
Steve Akinyemi <appcypher@outlook.com>
2018-05-13 12:58:05 +0100

    Make mac build easier

---
+++ build/SimpleOnlyBuild-Mac.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SIMPLE_DEBUG_VERSION=s0.3.303-debug
+PWD=$(pwd)
+
+cd build
+if [ -e ../src/makefiles/Makefile-Mac.mk ]; then
+	cd ../src/makefiles
+	echo "SIMPLE $SIMPLE_DEBUG_VERSION build"
+	echo "       Building simple and simple.dylib ..."
+    echo "       Build completed!"
+	make -s -f Makefile-Mac.mk
+    rm -rf ../sources/*.o
+	mkdir -p ../../dist
+	mv "./simple.dylib" "../../dist/simple.dylib"
+	mv "./a.out" "../../dist/simple"
+    # Copy executable and dynamic library to directories where it can be acessed directly from the shell.
+    cp "../../dist/simple.dylib" "/usr/local/lib/simple.dylib"
+    cp "../../dist/simple" "/usr/local/bin/simple"
+	cd $PWD
+    echo "Type \`simple\` and enter to begin using the compiler"
+else
+	echo "SIMPLE $SIMPLE_DEBUG_VERSION build"
+	echo "       The file `Makefile-Mac.mk` does not exist"
+	echo "       Skipping simple build"
+fi
