1596886f5aa24b91f295499c4b9c1e7d5d5b7679
Azeez Adewale <azeezadewale98@gmail.com>
2018-06-24 19:10:16 +0100

    --c flag to configure build dependencies and install if absent

--- build/Linux-Build.sh
+++ build/Linux-Build.sh
@@ -88,7 +88,12 @@ if [ $EXEC_TYPE = "configure" ] || [ $EXEC_TYPE = "install-configure" ]; then
 	echo "============================================================="
 	echo "simple-lang:configure: configure build $SIMPLE_DEBUG_VERSION"
 	echo "============================================================="
-	
+
+	dpkg -s "gcc" >/dev/null 2>&1 && {
+        echo "gcc is installed."
+    } || {
+        echo "gcc is not installed."
+    }
 	if [ $EXEC_TYPE = "install-configure" ]; then
 		EXEC_TYPE="install"
 	else
