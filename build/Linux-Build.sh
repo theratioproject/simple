5815fe86df2631b41045ae6324664047073677be
Azeez Adewale <azeezadewale98@gmail.com>
2018-06-24 20:20:04 +0100

    --c flag to configure build dependencies and install if absent

--- build/Linux-Build.sh
+++ build/Linux-Build.sh
@@ -121,7 +121,8 @@ if [ $EXEC_TYPE = "configure" ] || [ $EXEC_TYPE = "install-configure" ]; then
 					sudo apt install libcurl4-nss-dev
 					sudo apt install libcurl4-openssl-dev
 				else
-
+					echo "simple-lang:configure: package cannot be determined"
+					sudo apt install $1 exit
 				fi
 			fi
 		}
