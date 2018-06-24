ecd08d498532e2c6210fa0187e7555af1cbb4ee4
Azeez Adewale <azeezadewale98@gmail.com>
2018-06-24 19:41:24 +0100

    --c flag to configure build dependencies and install if absent

--- build/Linux-Build.sh
+++ build/Linux-Build.sh
@@ -91,7 +91,7 @@ if [ $EXEC_TYPE = "configure" ] || [ $EXEC_TYPE = "install-configure" ]; then
 	DEPENDENCIES='g++ gcc curl-config'
 	for i in $DEPENDENCIES; do
 		echo "simple-lang:configure: checking if $i is installed"
-		dpkg -s $i >/dev/null 2>&1 && {
+		ldconfig -p | grep $i >/dev/null 2>&1 && {
         	echo "simple-lang:configure: $i is installed on this machine."
 		} || {
 			echo "simple-lang:configure: $i is not installed on this machine ."
