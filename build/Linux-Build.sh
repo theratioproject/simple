b5b46cb11a11690dd4a45473ffeab360a47452ab
Azeez Adewale <azeezadewale98@gmail.com>
2018-06-24 19:53:27 +0100

    --c flag to configure build dependencies and install if absent

--- build/Linux-Build.sh
+++ build/Linux-Build.sh
@@ -97,9 +97,9 @@ if [ $EXEC_TYPE = "configure" ] || [ $EXEC_TYPE = "install-configure" ]; then
 			echo "simple-lang:configure: $i is not installed on this machine ."
 			if [ $ALLOW_DEP_INSTALL = 0 ]; then
 				read -p "Do you want to continue?(yes/no) "
-				echo $1
+				echo "answer is $1"
 			else
-
+				echo ""
 			fi
 		}
 	done
