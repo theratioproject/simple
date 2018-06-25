66773f499d8fe3db98b11bec37f98300ca0bc80d
Azeez Adewale <azeezadewale98@gmail.com>
2018-06-25 05:30:58 +0100

    script the simplepad.desktop to the system menu

--- build/Linux-Build.sh
+++ build/Linux-Build.sh
@@ -585,9 +585,9 @@ echo "SIMPLE_PATH=\"/simple/\"" >> /etc/environment
 	sudo link $DESTDIR/$PREFIX/bin/simplepad ~/Desktop/simplepad
 
 	echo "============================================================="
-	echo "simple-lang:link: add simple pad to the system menu"
+	echo "simple-lang:link: add simplepad to the system menu"
 	echo "============================================================="
-	echo "" >> 	
+	echo "" >> 
 
 	echo "============================================================="
 	echo "simple-lang:build: testing installtion > simple"
