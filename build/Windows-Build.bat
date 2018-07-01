0823a079129d1d3ae6c29a29d6136080a3d843df
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 00:58:23 +0100

    batch label to move simple* to installation folder

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -306,7 +306,6 @@ REM simple.exe and simple.dll has been successful create and copy executable to 
 	echo Copying Executable and building %VERSION% and %SIMPLE_DEBUG_VERSION% 
 	if !EXEC_TYPE!=="install" (
 		echo installation folder !INSTALLATION_FOLDER!\%VERSION%\
-		mkdir !INSTALLATION_FOLDER!\%VERSION%\
 		exit /b 0
 		if exist "!INSTALLATION_FOLDER!\%VERSION%\bin\" (
 			echo simple/bin: the ..\..\%SIMPLE_DEBUG_VERSION%\bin\ directory already exist
