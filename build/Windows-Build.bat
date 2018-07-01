9aafd30d1b2d7fabeac49501baf987dc02728d8c
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 19:07:34 +0100

    upgrade version from s0.3.34 to s0.3.35

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -696,7 +696,7 @@ exit /b
 	if !BUILD_ARC!=="x86" (
 		if exist "!MVS!\VC\vcvarsall.bat" (
 			echo simple-lang:configure:buildtool found !MVS!
-			call:callmsvisualstudio !MVS!\VC\vcvarsall.bat
+			call:callmsvisualstudio "!MVS!\VC\vcvarsall.bat"
 			exit /b 0
 		) else (
 			echo simple-lang:configure:buildtool Microsoft Visual Studio not found
@@ -707,7 +707,7 @@ exit /b
 	) else (
 		if exist "!MVS!\VC\vcvarsall.bat" (
 			echo simple-lang:configure:buildtool found !MVS!
-			call:callmsvisualstudio !MVS!\VC\vcvarsall.bat
+			call:callmsvisualstudio "!MVS!\VC\vcvarsall.bat"
 			exit /b 0
 		) else (
 			echo simple-lang:configure:buildtool resolve to 32 bit
