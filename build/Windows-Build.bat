99a84063177902b2474091f6c381277b6ea853a1
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 00:26:04 +0100

    batch label to move simple* to installation folder

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -278,7 +278,9 @@ exit /b %ERRORLEVEL%
 		cl.exe /D_USRDLL /D_WINDLL /TC !SIMPLE_C_FILES! /MT /I %~dp0\..\simple\includes\ /link /DLL /OUT:%~dp0\..\simple\dist\simple.dll libcmt.lib
 		call:deletetempfiles *.obj *.exp *.cod
 		REM move *.lib %~dp0\..\simple\dist
-		if %~dp0\..\simple\dist\simple.dll
+		if exist "..\simple\dist\simple.dll" (
+			cl.exe
+		)
 		exit /b 0
 	) else (
 		if exist "..\simple\makefiles\Makefile-Windows.mk" (
