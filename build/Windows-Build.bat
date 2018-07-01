958329ae77294443fb57aaab97402576e97f85e7
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 03:34:59 +0100

    enable parameter in Windows-Build.bat build file

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -89,7 +89,7 @@ exit /b %ERRORLEVEL%
 	if exist "C:/MinGW/bin/gccs.exe" (
 		echo simple-lang:configure found MinGW Build Toolchain
 	) else (
-		gcc >nul (
+		gccs >nul (
 		  echo "found"
 		) || (
 		  echo "not found "
