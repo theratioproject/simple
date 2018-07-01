b5e6e1ffa6f4fdcb20179b0e51a7a28c1fa4446f
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 22:29:22 +0100

    make build rule for Visual Studio simple.dll simple.exe and simplew.exe

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -275,7 +275,7 @@ exit /b %ERRORLEVEL%
 		echo yea Visual Studio and flies are !SIMPLE_C_FILES!
 		mkdir %~dp0\..\simple\dist
 		cl.exe /D_USRDLL /D_WINDLL /LD /Tc !SIMPLE_C_FILES! /MT /link /DLL /OUT:%~dp0\..\simple\dist\heloworld.dll
-		call:deletetempfiles *.obj *.exp
+		call:deletetempfiles *.obj *.exp *.cod
 		move *.lib %~dp0\..\simple\dist
 	) else (
 		if exist "../simple/makefiles/Makefile-Windows.mk" (
