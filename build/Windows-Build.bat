d9ef8205d07415baf37ebee7b400caa0801603bd
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 22:00:47 +0100

    make build rule for Visual Studio simple.dll simple.exe and simplew.exe

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -684,7 +684,7 @@ exit /b
 
 :getsimplecfiles
 	if exist "..\simple\sources\" (
-		SET SIMPLE_C_FILES="%~dp0\..\simple\sources\simple_api.c"
+		SET SIMPLE_C_FILES="%~dp0\..\simple\sources\simple_api.c %~dp0\..\simple\sources\simple_codegen.c"
 	) else (
 		SET SIMPLE_C_FILES="%~dp0\none\*.c"
 	)
