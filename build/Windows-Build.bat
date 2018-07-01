dfb927b2b4c9c9229694043724f70e5e721753eb
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 15:39:23 +0100

    configuration for windows in Windows-Build.bat build file

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -93,7 +93,7 @@ exit /b %ERRORLEVEL%
 	echo simple-lang:configure preparing to find build toolchain...
 	echo simple-lang:configure checking if gcc is present in path
 	gcc 2> ../../simple_build_configure
-	SET gccval < ../../simple_build_configure
+	SET gccval=../../simple_build_configure
 	exit /b 0
 	echo simple-lang:configure serching for mingw and msys
 	if exist "C:\MinGW\msys\1.0\bin\" (
