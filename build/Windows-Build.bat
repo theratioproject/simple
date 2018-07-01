9a458395b38160a40f4aac34e338a9288f459154
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 18:36:40 +0100

    upgrade version from s0.3.34 to s0.3.35

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -675,16 +675,16 @@ if exist "../simple/dist/" (
 exit /b
 
 :locatevisualstudio
-	echo param !1!
+	echo param %1
 	echo simple-lang:configure:buildtool Microsoft Visual Studio is specified
 	echo simple-lang:configure:buildtool searching for Microsoft Visual Studio
-	if "!1!"=="" (
+	if "%1"=="" (
 		SET "PROGRAMFILESPATH=%ProgramFiles%"
 	)
-	if "!1!"=="x64" (
+	if "%1"=="x64" (
 		SET "PROGRAMFILESPATH=%ProgramFiles%"
 	)
-	if "!1!"=="x86" (
+	if "%1"=="x86" (
 		SET "PROGRAMFILESPATH=%ProgramFiles(x86)%"
 	)
 	echo patter %PROGRAMFILESPATH%
