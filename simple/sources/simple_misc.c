53e425557bacec7ee09bf70cb9fbd2c4c6934f61
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 22:24:55 +0100

    make build rule for Visual Studio simple.dll simple.exe and simplew.exe

--- simple/sources/simple_misc.c
+++ simple/sources/simple_misc.c
@@ -138,7 +138,7 @@ const char *change_file_ext(const char *absolute_name, const char *extension){
     return name;
 }
 
-void simple_showtime ( clock_t before_execution, clock_t after_execution )
+void simple_showtime ( clock_t before_execution)
 {
     after_execution = clock();
     float seconds = get_time_different((float)(after_execution - before_execution) / CLOCKS_PER_SEC);
