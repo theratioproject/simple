bac4a3363eacb1ca3b1b36f8083df1d294630afe
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 00:04:50 +0100

    make *.c sources clearly compatible with Visual Studio

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -311,9 +311,9 @@ REM simple.exe and simple.dll has been successful create and copy executable to 
 		)
 		if exist "../simple/dist/simple.exe" (
 			echo simple: copying simple.exe and simple.dll to ../../%SIMPLE_DEBUG_VERSION%/bin directory
-			cp ..\simple\dist\simple.exe ../../%SIMPLE_DEBUG_VERSION%/bin
-			cp ..\simple/dist\simplew.exe ../../%SIMPLE_DEBUG_VERSION%/bin
-			cp ..\simple/dist\simple.dll ../../%SIMPLE_DEBUG_VERSION%/bin
+			copy ..\simple\dist\simple.exe ../../%SIMPLE_DEBUG_VERSION%/bin
+			copy ..\simple/dist\simplew.exe ../../%SIMPLE_DEBUG_VERSION%/bin
+			copy ..\simple/dist\simple.dll ../../%SIMPLE_DEBUG_VERSION%/bin
 		) else (
 			echo error:simple: build fails simple.exe and simple.dll cannot be found
 			echo error:simple: try rebuilding individually
