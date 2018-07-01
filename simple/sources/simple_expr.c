b8165ce627aae098c48fc9286bb3dbc0273cabc1
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 21:22:33 +0100

    make build rule for Visual Studio simple.dll simple.exe and simplew.exe

--- simple/sources/simple_expr.c
+++ simple/sources/simple_expr.c
@@ -951,8 +951,7 @@ int simple_parser_factor ( Parser *parser,int *nFlag )
 	}
 	/* Factor --> New Identifier */
 	if ( simple_parser_iskeyword(parser,KEYWORD_NEW) ) {
-		simple_parser_nexttoken(parser); char *class_name ;
-		class_name = parser->TokenText;
+		simple_parser_nexttoken(parser); const char *class_name = parser->TokenText;
 		SIMPLE_PARSER_IGNORENEWLINE ;
 		/* Generate Code */
 		simple_parser_icg_newoperation(parser,ICO_NEWOBJ); 
