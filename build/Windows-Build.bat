874bb3901e08b08e07d202206de0970b0b25519e
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 16:53:44 +0100

    configuration for windows in Windows-Build.bat build file

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -240,7 +240,6 @@ exit /b %ERRORLEVEL%
 	exit /b 0
 
 :installdebug
-	echo !THERE_IS_VS!
 	if !EXEC_TYPE!=="install" (
 		echo =============================================================
 		echo simple-lang:install: install simple-lang %VERSION%
