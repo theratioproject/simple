3fceed59bedad2f73a27d3df96dc5e0b4a605507
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 16:40:06 +0100

    configuration for windows in Windows-Build.bat build file

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -245,10 +245,10 @@ exit /b %ERRORLEVEL%
 		echo =============================================================
 	)
 	REM Remove previous build of the current versions
-	if exist "../../%SIMPLE_DEBUG_VERSION%/" (
+	if exist "..\..\%SIMPLE_DEBUG_VERSION%/" (
 		echo a previous simple build %SIMPLE_DEBUG_VERSION% is detected
 		echo removing previous build and performing a clean build
-		rm -r ../../%SIMPLE_DEBUG_VERSION%/
+		rm -r ..\..\%SIMPLE_DEBUG_VERSION%\
 	) 
 	echo simple-lang: !THERE_IS_VS!
 	exit /b 0
