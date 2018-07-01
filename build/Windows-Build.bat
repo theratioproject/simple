3dcae6215074899071cee0eb10f0e345674e1ab9
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 23:31:25 +0100

    make *.c sources clearly compatible with Visual Studio

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -684,7 +684,7 @@ exit /b
 
 :getsimplecfiles
 	if exist "..\simple\sources\" (
-		SET SIMPLE_C_FILES="%~dp0\..\simple\sources\simple_vmperformance.c"
+		SET SIMPLE_C_FILES="%~dp0\..\simple\sources\simple_vmstackvars.c"
 	) else (
 		SET SIMPLE_C_FILES="%~dp0\none\*.c"
 	)
