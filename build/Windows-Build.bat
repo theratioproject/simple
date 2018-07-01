4baae4618fbee16b247c4790bda2aa8793c4dc18
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 00:02:30 +0100

    make *.c sources clearly compatible with Visual Studio

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -295,6 +295,8 @@ exit /b %ERRORLEVEL%
 	exit /b 0
 	
 	
+REM simple.exe and simple.dll has been successful create and copy executable to %SIMPLE_DEBUG_VERSION% directory
+
 :copysimpledllexe
 	echo Copying Executable and building %SIMPLE_DEBUG_VERSION%
 	if !EXEC_TYPE!=="install" (
@@ -305,7 +307,6 @@ exit /b %ERRORLEVEL%
 	)
 	exit /b 0
 
-REM simple.exe and simple.dll has been successful create and copy executable to %SIMPLE_DEBUG_VERSION% directory
 	
 
 
