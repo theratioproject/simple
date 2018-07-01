3d29f9aae9db320e61cfb43251606dbd28cb8bd9
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 00:12:07 +0100

    batch label to move simple* to installation folder

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -275,7 +275,7 @@ exit /b %ERRORLEVEL%
 		call:getsimplecfiles
 		echo yea Visual Studio and flies are !SIMPLE_C_FILES!
 		mkdir %~dp0\..\simple\dist
-		cl.exe /D_USRDLL /D_WINDLL /TC !SIMPLE_C_FILES! /MT /I %~dp0\..\simple\includes\ /link /DLL /OUT:%~dp0\..\simple\dist\simple.dll
+		cl.exe /D_USRDLL /D_WINDLL /TC !SIMPLE_C_FILES! /MT /I %~dp0\..\simple\includes\ /link /DLL /OUT:%~dp0\..\simple\dist\simple.dll libcmt.lib
 		call:deletetempfiles *.obj *.exp *.cod
 		REM move *.lib %~dp0\..\simple\dist
 		exit /b 0
