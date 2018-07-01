505c0dd2a1292db68c1f251f628bbd796f1b018b
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 17:13:09 +0100

    configuration for windows in Windows-Build.bat build file

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -103,6 +103,7 @@ exit /b %ERRORLEVEL%
 	echo =============================================================
 	echo simple-lang:configure: configure build %VERSION%
 	echo =============================================================
+	echo simple-lang:configure:buildtool determining if a specific tool is specified
 	echo simple-lang:configure preparing to find build toolchain...
 	echo simple-lang:configure:compiler checking if gcc is present in path
 	gcc 2> ..\..\simple_build_configure
