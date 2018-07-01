2a32de604ce8e0a13b6f2271362d3002ce4252a0
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 02:42:00 +0100

    enable parameter in Windows-Build.bat build file

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -1,20 +1,30 @@
-@echo off
+echo off
+cls 
 
-echo.going to execute myDosFunc with different arguments
-call:myDosFunc 100 YeePEE
-call:myDosFunc 100 "for me"
-call:myDosFunc 100,"for me"
-call:myDosFunc 100,for me
+SET EXEC_TYPE="install"
+SET VER="0.3.34"
 
-echo.&pause&goto:eof
+for %%x in (%*) do (
+	if %%x=="--install" (
+		SET EXEC_TYPE="install"
+	) 
+	if %%x=="-i" (
+		SET EXEC_TYPE="install"
+	)
+	if %%x=="--uninstall" (
+		call:uninstall
+	) 
+	if %%x=="-u" (
+		call:uninstall
+	)
+)
 
+:: Functions
 
-::--------------------------------------------------------
-::-- Function section starts below here
-::--------------------------------------------------------
+:install
+	echo Installation Type : %EXEC_TYPE%
+	exit /b 0
 
-:myDosFunc    - here starts my function identified by it's label
-echo.
-echo. here the myDosFunc function is executing a group of commands
-echo. it could do %~1 of things %~2.
-goto:eof
+:uninstall
+	echo unintstalling
+	exit /b 0
