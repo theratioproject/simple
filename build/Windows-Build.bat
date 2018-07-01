7b26285397fbf4287fa68b9fee34876ed09eed96
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-01 03:51:47 +0100

    enable parameter in Windows-Build.bat build file

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -85,7 +85,7 @@ exit /b %ERRORLEVEL%
 	echo simple-lang:configure: configure build %SIMPLE_DEBUG_VERSION%
 	echo =============================================================
 	echo simple-lang:configure creating dummy C file
-	echo int main() {} temp_simple_configure.c
+	echo 'int main() {}' temp_simple_configure.c
 	echo simple-lang:configure preparing to find build toolchain...
 	echo simple-lang:configure serching for mingw and msys
 	if exist "C:/MinGW/bin/gccs.exe" (
