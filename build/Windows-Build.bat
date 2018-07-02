c171a001577e376b0580a2987f6da7b0b39836b0
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 21:43:00 +0100

    add flags for standalone builds

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -584,15 +584,6 @@ REM BULDING DYNAMIC LIBRARIES
 		) else (
 			call:dynamicmoduleserror
 		)
-		if exist "..\simple\includes" (
-			echo includes: copying includes to ..\..\%SIMPLE_DEBUG_VERSION%\ directory
-			call:deletedirectories "..\..\%SIMPLE_DEBUG_VERSION%\includes"
-			xcopy "..\simple\includes" "..\..\%SIMPLE_DEBUG_VERSION%\includes" /s /h /e /i /k /f /c
-		) else (
-			echo error:includes: the includes directory cannot be found
-			echo error:includes: the repository appears to be currupted. 
-			echo error:includes: try clonning the simple repository again to resolve the issue
-		)
 	)
 
 	exit /b 0
