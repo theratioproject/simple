84c81fa2ba84a4389aa8a3f9f1ec28ddbb54c43b
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 13:25:20 +0100

    include 64 bits dependencies for windows

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -402,7 +402,7 @@ REM RESOLVE DEPENDENCIES
 		echo dependencies: ssleay32.dll
 	if !BUILD_ARC!=="x64" (
 		if exist "..\modules\dynamic_modules\security\bin\ssleay64.dll" (
-			movedependencytobin ssleay32.dll ..\modules\dynamic_modules\security\bin\ssleay32.dll
+			movedependencytobin ssleay64.dll ..\modules\dynamic_modules\security\bin\ssleay64.dll
 			
 		) else (
 			call:dependencieserror ssleay32.dll
