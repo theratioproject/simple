db6f0acf4f003932353a4847cddda195ee77f9cd
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 11:26:46 +0100

    include option to keep build in source tree

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -278,7 +278,7 @@ exit /b %ERRORLEVEL%
 		if exist "..\..\%SIMPLE_DEBUG_VERSION%\" (
 			echo a previous simple build %SIMPLE_DEBUG_VERSION% is detected
 			echo removing previous build and performing a clean build
-			rmdir /S /Q ..\..\%SIMPLE_DEBUG_VERSION%\
+			call:deletedirectories ..\..\%SIMPLE_DEBUG_VERSION%\
 		) 
 	) 
 	call:buildsimpledllexe
