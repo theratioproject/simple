f698d18e2ce2c17722e082c040ae3dd7fc86d4e3
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-02 22:02:35 +0100

    add flags for standalone builds

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -537,7 +537,8 @@ REM BULDING DYNAMIC LIBRARIES
 	if !THERE_IS_VS!=="true" (
 		echo building archiver dynamic module
 		call:getdynamicmodulefiles archiver
-		cl.exe /D_USRDLL /D_WINDLL /TC !DYNAMIC_MODULE_FILES! /MT /I !DY_MODULE_INCLUDE_DIR! /link /DLL /OUT:%~dp0\..\modules\dynamic_modules\dist\archiver.dll
+		echo dy files are !DY_MODULE_FILES! dy includes dir !DY_MODULE_INCLUDE_DIR!
+		REM cl.exe /D_USRDLL /D_WINDLL /TC !DY_MODULE_FILES! /MT /I !DY_MODULE_INCLUDE_DIR! /link /DLL /OUT:%~dp0\..\modules\dynamic_modules\dist\archiver.dll
 	) else (
 		if exist "..\modules" (
 			cd "..\modules"
