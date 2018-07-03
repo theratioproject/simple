66d47c7b681b1ba511bf94642daeeca6ba98cf4f
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-03 16:38:37 +0100

    format dynamic modules sources to be compatible with Visual Studio Compiler

--- modules/dynamic_modules/parser/includes/json_inttypes.h
+++ modules/dynamic_modules/parser/includes/json_inttypes.h
@@ -12,8 +12,14 @@ typedef __int64 int64_t;
 #else
 
 #ifdef HAVE_INTTYPES_H
-#include "stdint.h"
-#include "inttypes.h"
+typedef signed char  int8_t;
+typedef signed short int16_t;
+typedef signed int   int32_t;
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+typedef signed long long   int64_t;
+typedef unsigned long long uint64_t;
 #endif
 /* inttypes.h includes stdint.h */
 
