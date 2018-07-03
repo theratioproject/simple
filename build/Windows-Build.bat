a90f4aa2ce78219963e2cfc54cfe2f688eaaadca
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-03 12:38:47 +0100

    allow x86 and x64 build of dynamic modules for windows

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -897,10 +897,12 @@ exit /b
 	
 	exit /b 0
 
-:getdynamicmodulefiles 
+:getdynamicmodulefiles
 	if exist "..\modules\dynamic_modules\" (
-		SET DY_MODULE_INCLUDE_DIR="%~dp0\..\modules\dynamic_modules\%2\"
-		SET DY_MODULE_FILES="%~dp0\..\modules\dynamic_modules\%2\*.%1"
+		if %1=="archiver" (
+			SET DY_MODULE_INCLUDE_DIR="%~dp0\..\modules\dynamic_modules\archiver\"
+			SET DY_MODULE_FILES="%~dp0\..\modules\dynamic_modules\archiver\*.c"
+		)
 	) else (
 		call:repocurrupterror dynamic_modules
 	)
