77d2d295271041e82ca74d0f9f246c44170eda84
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-03 13:34:39 +0100

    allow x86 and x64 build of dynamic modules for windows

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -535,8 +535,7 @@ REM BULDING DYNAMIC LIBRARIES
 		call:deletedirectories %~dp0\..\modules\dynamic_modules\dist\
 	)
 	if !THERE_IS_VS!=="true" (
-		echo building consoler dynamic module
-		
+		call:getdynamicmodulefiles c file_savant
 	) else (
 		if exist "..\modules" (
 			cd "..\modules"
