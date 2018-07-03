088b9ddc8f3a8cf2e032ec1aec448150919e1965
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-03 14:18:14 +0100

    allow x86 and x64 build of dynamic modules for windows

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -571,7 +571,6 @@ REM BULDING DYNAMIC LIBRARIES
 	echo building %2 dynamic module
 	call:getdynamicmodulefiles %1 %2
 	cl.exe /D_USRDLL /D_WINDLL /LD /MT !DY_MODULE_FILES! /link /DLL /LIBPATH:%~dp0\..\simple\dist\ simple.lib 
-	echo cl.exe /D_USRDLL /D_WINDLL /LD /MT !DY_MODULE_FILES! /link /DLL /LIBPATH:%~dp0\..\simple\dist\ simple.lib 
 	call:deletetempfiles *.obj *.exp *.cod
 		
 	exit /b 0
