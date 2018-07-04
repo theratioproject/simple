06700fcd5587471645a92d79af8fc861f8b055ad
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 23:54:52 +0100

    minor compatibility for web design - cgi

--- simple/sources/simple_misc.c
+++ simple/sources/simple_misc.c
@@ -28,12 +28,12 @@ SIMPLE_API void print_line ( void )
 }
 
 void display_help(){
-    printf("Usage:  simple ([OPTIONS]) [PATH] [ARGS...]\n");
+    printf("Usage:  simple [FLAGS] [PATH] [ARGS...]\n");
     printf("\t\t\t<to run a simple or complex file>\n\n");
     printf("[PATH]     : The Simple/Complex source file to execute.\n");
     printf("[ARG...]   : The arguments to pass to your simple application.\n");
-    printf("([OPTIONS]): The flags/options to pass to the Simple VM/Simplify.\n\n");
-    printf("The OPTIONS include : \n");
+    printf("[FLAGS]: The flags/options to pass to the Simple VM/Simplify.\n\n");
+    printf("The FLAGS include : \n");
     printf("  -h --help                    Displays this help and exits.\n");
     printf("  -k --show-tokens             Scans and outputs the tokens in the source file.\n");
     //printf("  -s -simplify                Compile simple file to one complex file.\n");
