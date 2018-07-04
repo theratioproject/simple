19d7e9d4296a2349a0cf53373643d68477b6f3cb
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 12:01:36 +0100

    resolve simple standard module for x86 and x64 build

---
+++ environment/modular/modular.sim.bake.c.o.d
@@ -0,0 +1,44 @@
+..\environment\modular\modular.sim.bake.c.o: \
+ ..\environment\modular\modular.sim.bake.c \
+ c:\simple\s0.3.35\includes\simple.h \
+ c:\simple\s0.3.35\includes\simple_misc.h \
+ c:\simple\s0.3.35\includes\simple_string.h \
+ c:\simple\s0.3.35\includes\simple_item.h \
+ c:\simple\s0.3.35\includes\simple_list.h \
+ c:\simple\s0.3.35\includes\simple_hashtable.h \
+ c:\simple\s0.3.35\includes\simple_state.h \
+ c:\simple\s0.3.35\includes\simple_scanner.h \
+ c:\simple\s0.3.35\includes\simple_parser.h \
+ c:\simple\s0.3.35\includes\simple_codegen.h \
+ c:\simple\s0.3.35\includes\simple_vm.h \
+ c:\simple\s0.3.35\includes\simple_vmgc.h \
+ c:\simple\s0.3.35\includes\simple_api.h \
+ c:\simple\s0.3.35\includes\simple_vmdll.h
+
+c:\simple\s0.3.35\includes\simple.h:
+
+c:\simple\s0.3.35\includes\simple_misc.h:
+
+c:\simple\s0.3.35\includes\simple_string.h:
+
+c:\simple\s0.3.35\includes\simple_item.h:
+
+c:\simple\s0.3.35\includes\simple_list.h:
+
+c:\simple\s0.3.35\includes\simple_hashtable.h:
+
+c:\simple\s0.3.35\includes\simple_state.h:
+
+c:\simple\s0.3.35\includes\simple_scanner.h:
+
+c:\simple\s0.3.35\includes\simple_parser.h:
+
+c:\simple\s0.3.35\includes\simple_codegen.h:
+
+c:\simple\s0.3.35\includes\simple_vm.h:
+
+c:\simple\s0.3.35\includes\simple_vmgc.h:
+
+c:\simple\s0.3.35\includes\simple_api.h:
+
+c:\simple\s0.3.35\includes\simple_vmdll.h:
