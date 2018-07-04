28f7a8d330a3c01dad1fe4fd025f11f61a37d728
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 21:16:55 +0100

    minor compatibility for web design - cgi

--- simple/sources/simple_vm.c
+++ simple/sources/simple_vm.c
@@ -1176,6 +1176,76 @@ SIMPLE_API void simple_vm_showerrormessage ( VM *vm,const char *cStr )
 	}
 }
 
+SIMPLE_API void simple_vm_cgi_showerrormessage ( VM *vm,const char *cStr )
+{
+	int x,lBlockCall,is_last_block  ;
+	List *list  ;
+	const char *cFile  ;
+	/* Print the Error Message */
+	printf( "\nLine %d -> %s \n",vm->nLineNumber,cStr ) ;
+	/* Print Calling Information */
+	lBlockCall = 0 ; is_last_block = 1 ;
+	for ( x = simple_list_getsize(vm->pBlockCallList) ; x >= 1 ; x-- ) {
+		list = simple_list_getlist(vm->pBlockCallList,x);
+		/*
+		**  If we have ICO_LoadBlock but not ICO_CALL then we need to pass 
+		**  ICO_LOADBLOCK is executed, but still ICO_CALL is not executed! 
+		*/
+		if ( simple_list_getsize(list) < SIMPLE_BLOCKCL_CALLERPC ) {
+			continue ;
+		}
+		if ( simple_list_getint(list,SIMPLE_BLOCKCL_TYPE) == SIMPLE_BLOCKTYPE_SCRIPT ) {
+			/*
+			**  Prepare Message 
+			**  In 
+			*/
+			if (is_last_block == 1) { printf("\tat "); is_last_block = 0; } else { printf("at "); }
+			/* Method or Block */
+			/*if ( simple_list_getint(list,SIMPLE_BLOCKCL_METHODORBLOCK) ) {
+				printf( "method " ) ;
+			}
+			else {
+				printf( "block " ) ;
+			}*/
+			/* Block Name */
+			printf( "%s",simple_list_getstring(list,SIMPLE_BLOCKCL_NAME) ) ;
+			/* Adding () */
+			printf( "() in file " ) ;
+			/* File Name */
+			if ( lBlockCall == 1 ) {
+				cFile = (const char *) simple_list_getpointer(list,SIMPLE_BLOCKCL_NEWFILENAME) ;
+			}
+			else {
+				if ( vm->nInClassRegion ) {
+					cFile = vm->cFileNameInClassRegion ;
+				}
+				else {
+					cFile = vm->file_name ;
+				}
+			}
+			printf( "%s",file_real_name(cFile) ) ;
+			/* Called From */
+			printf( "\n\tat line %d ",simple_list_getint(list,SIMPLE_BLOCKCL_LINENUMBER) ) ;
+			lBlockCall = 1 ;
+		}
+		else {
+			printf( "In %s ",file_real_name(simple_list_getstring(list,SIMPLE_BLOCKCL_NAME)) ) ;
+		}
+	}
+	if ( lBlockCall ) {
+		printf( "in file %s ",file_real_name(simple_list_getstring(vm->sState->files_list,1) )) ;
+	}
+	else {
+		if ( vm->nInClassRegion ) {
+			cFile = vm->cFileNameInClassRegion ;
+		}
+		else {
+			cFile = file_real_name(vm->file_name) ;
+		}
+		printf( "\tin file %s ",cFile ) ;
+	}
+}
+
 void simple_vm_setfilename ( VM *vm )
 {
 	if ( vm->nInClassRegion ) {
