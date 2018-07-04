680cb2ea9eb0d0b8b387d8c18610901af727c40d
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 06:09:29 +0100

    resolve simple standard module for x86 and x64 build

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -715,31 +715,24 @@ REM COPY THE SIMPLE MODULES
 
 	exit /b 0
 	
-REM COPY ALL THE MODULE IN LOOP MODE
+REM COPY ALL THE MODULE IN LOOP MODE 
 	
 :copymodulesinloop
 	for %%x in (%*) do (
-		echo modules: %1 module
-		if exist "..\modules\%1" (
+		echo modules: %%x module
+		if exist "..\modules\%%x" (
 			if !EXEC_TYPE!=="install" (
-				echo modules: copying %1 module to ../../%SIMPLE_DEBUG_VERSION%/modules directory
-				xcopy "../modules/%1" "%~dp0\..\..\%SIMPLE_DEBUG_VERSION%\modules\%1\" /s /h /e /k /f /c
+				echo modules: copying %%x module to ../../%SIMPLE_DEBUG_VERSION%/modules directory
+				xcopy "../modules/%%x" "%~dp0\..\..\%SIMPLE_DEBUG_VERSION%\modules\%%x\" /s /h /e /k /f /c
 			)
 			if !EXEC_TYPE!=="install" (
-				echo modules: copying %1 module to ../../%SIMPLE_DEBUG_VERSION%/modules directory
-				xcopy "../modules/%1" "%~dp0\..\..\%SIMPLE_DEBUG_VERSION%\modules\%1\" /s /h /e /k /f /c
+				echo modules: copying %%x module to ../../%SIMPLE_DEBUG_VERSION%/modules directory
+				xcopy "../modules/%%x" "%~dp0\..\..\%SIMPLE_DEBUG_VERSION%\modules\%%x\" /s /h /e /k /f /c
 			)
 		) else (
-			call:modulecurrupterror %1
+			call:modulecurrupterror %%x
 		)
 	)
-
-	exit /b 0
-	
-REM COPY SINGLE MODULE TO DIST FOLDER
-	
-:copysinglesimplemodule	
-	
 
 	exit /b 0
 	
