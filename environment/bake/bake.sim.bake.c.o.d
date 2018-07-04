e7e804d40277a2b1400fd515f475d7a9aca45445
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 09:28:31 +0100

    resolve simple standard module for x86 and x64 build

---
+++ environment/bake/bake.sim.bake.c.o.d
@@ -0,0 +1,44 @@
+..\environment\bake\bake.sim.bake.c.o: \
+ ..\environment\bake\bake.sim.bake.c \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_misc.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_string.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_item.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_list.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_hashtable.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_state.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_scanner.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_parser.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_codegen.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_vm.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_vmgc.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_api.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_vmdll.h
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_misc.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_string.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_item.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_list.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_hashtable.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_state.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_scanner.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_parser.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_codegen.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_vm.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_vmgc.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_api.h:
+
+c:\users\thecarisma\documents\simple_github\s0.3.35-debug\includes\simple_vmdll.h:
