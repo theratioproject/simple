47de2650fee3305a27331c116b294b5320967718
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-05 00:36:24 +0100

    minor compatibility for web design - cgi

--- simple/sources/simple_vmstackvars.c
+++ simple/sources/simple_vmstackvars.c
@@ -37,15 +37,15 @@ void simple_vm_pushv ( VM *vm )
 			if ( vm->nInClassRegion == 0 ) {
 				if ( simple_list_getint(var,SIMPLE_VAR_TYPE) == SIMPLE_VM_NULL ) {
 					if ( simple_list_isstring(var,SIMPLE_VAR_VALUE) ) {
-						/*if ( strcmp(simple_list_getstring(var,SIMPLE_VAR_VALUE),"NULL") == 0 ) {
+						if ( strcmp(simple_list_getstring(var,SIMPLE_VAR_VALUE),"NULL") == 0 ) {
 							simple_vm_error2(vm,SIMPLE_VM_ERROR_USINGNULLVARIABLE,simple_list_getstring(var,SIMPLE_VAR_NAME));
 							if ( simple_list_getlist(vm->pActiveMem,simple_list_getsize(vm->pActiveMem)) == var ) {
-								/* Delete the Item from the HashTable *
+								/* Delete the Item from the HashTable */
 								simple_hashtable_deleteitem(vm->pActiveMem->pHashTable,simple_list_getstring(var,SIMPLE_VAR_NAME));
 								simple_list_deletelastitem_gc(vm->sState,vm->pActiveMem);
 							}
 							return ;
-						} */
+						}
 					}
 				}
 			}
@@ -125,10 +125,10 @@ void simple_vm_assignment ( VM *vm )
 				simple_list_setstring2_gc(vm->sState,var, SIMPLE_VAR_VALUE , simple_string_get(string_one),simple_string_size(string_one));
 			} else {
 				/* Check NULL Variable */
-				/*if ( simple_list_getint(var,SIMPLE_VAR_TYPE) == SIMPLE_VM_NULL ) {
+				if ( simple_list_getint(var,SIMPLE_VAR_TYPE) == SIMPLE_VM_NULL ) {
 					simple_vm_error2(vm,SIMPLE_VM_ERROR_USINGNULLVARIABLE,simple_list_getstring(var,SIMPLE_VAR_NAME));
 					return ;
-				}*/
+				}
 				if ( simple_list_isstring(var,SIMPLE_VAR_VALUE) ) {
 					string = simple_list_getstringobject(var,SIMPLE_VAR_VALUE);
 					simple_string_add2_gc(vm->sState,string,simple_string_get(string_one),simple_string_size(string_one));
@@ -149,10 +149,10 @@ void simple_vm_assignment ( VM *vm )
 				simple_list_setdouble_gc(vm->sState,var, SIMPLE_VAR_VALUE , nNum1);
 			} else {
 				/* Check NULL Variable */
-				/*if ( simple_list_getint(var,SIMPLE_VAR_TYPE) == SIMPLE_VM_NULL ) {
+				if ( simple_list_getint(var,SIMPLE_VAR_TYPE) == SIMPLE_VM_NULL ) {
 					simple_vm_error2(vm,SIMPLE_VM_ERROR_USINGNULLVARIABLE,simple_list_getstring(var,SIMPLE_VAR_NAME));
 					return ;
-				}*/
+				}
 				simple_vm_beforeequallist(vm,var,nNum1);
 			}
 		}
