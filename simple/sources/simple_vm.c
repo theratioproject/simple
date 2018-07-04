0d7fff9009685f27713ee1d8f2b6a14276f3418e
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 23:14:06 +0100

    minor compatibility for web design - cgi

--- simple/sources/simple_vm.c
+++ simple/sources/simple_vm.c
@@ -1242,7 +1242,7 @@ SIMPLE_API void simple_vm_cgi_showerrormessage ( VM *vm,const char *cStr )
 	}
 	if ( lBlockCall ) {
 		printf( "in file %s ",file_real_name(simple_list_getstring(vm->sState->files_list,1) )) ;
-		printf("<td bgcolor='#eeeeec' align='center'>%s</td>",simple_list_getstring(vm->sState->files_list,1)) ;
+		printf("<td bgcolor='#eeeeec' align='center'> from file %s</td>",simple_list_getstring(vm->sState->files_list,1)) ;
 	}
 	else {
 		if ( vm->nInClassRegion ) {
@@ -1252,7 +1252,7 @@ SIMPLE_API void simple_vm_cgi_showerrormessage ( VM *vm,const char *cStr )
 			cFile = file_real_name(vm->file_name) ;
 		}
 		printf( "\tin file %s ",cFile ) ;
-		printf("<td bgcolor='#eeeeec' align='center'>%s</td>",cFile) ;
+		printf("<th bgcolor='#eeeeec' align='center'> from file %s</td>",cFile) ;
 	}
 	printf("\n</table>");
 }
