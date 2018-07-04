63d2094368ae5cddbb9d73a69480d1532e433681
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 19:53:59 +0100

    minor compatibility for web design - cgi

--- simple/sources/simple_scanner.c
+++ simple/sources/simple_scanner.c
@@ -904,8 +904,7 @@ SIMPLE_API void simple_execute ( char *file_name, int nISCGI,int nRun,int nPrint
 	sState->nWarning = nWarn ;
 	sState->argc = argc ;
 	sState->argv = argv ;
-	if (sState->nISCGI)
-		simple_state_cgiheader(sState);
+	
 	if ( is_simple_file(file_name) ) {
 		simple_scanner_readfile(sState,file_name);
 	}
