6520b7268bb1878a91fb548578ff8ee9a544e189
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-04 11:16:58 +0100

    resolve simple standard module for x86 and x64 build

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -377,6 +377,21 @@ exit /b %ERRORLEVEL%
 	)
 	exit /b 0
 	
+:locatecygwin
+	echo simple-lang:configure:compiler checking if cygwin is present in path C:/cygwin/
+	if exist "C:/cygwin/" (
+		echo simple-lang:configure found CygWIN Build Toolchain
+		if !EXEC_TYPE!=="configure" (
+			call:setcompilerenv C:\cygwin\bin\
+		) else (
+			SET PATH=!PATH!;C:\cygwin\bin\
+		) 
+	) else (
+		call:compilernotfound C:/cygwin/
+	)
+		
+	exit /b 0
+	
 :locategcc
 	echo simple-lang:configure:compiler checking if gcc is present in path
 	gcc 2> ..\..\simple_build_configure
@@ -411,21 +426,6 @@ exit /b %ERRORLEVEL%
 		call:deletetempfiles ..\..\simple_build_configure
 	)
 	
-	exit /b 0
-	
-:locatecygwin
-	echo simple-lang:configure:compiler checking if cygwin is present in path C:/cygwin/
-	if exist "C:/cygwin/" (
-		echo simple-lang:configure found CygWIN Build Toolchain
-		if !EXEC_TYPE!=="configure" (
-			call:setcompilerenv C:\cygwin\bin\
-		) else (
-			SET PATH=!PATH!;C:\cygwin\bin\
-		) 
-	) else (
-		call:compilernotfound C:/cygwin/
-	)
-		
 	exit /b 0
 	
 :setcompilerenv
