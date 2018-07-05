13ed27bf76c2a0ecf8f8c17701d2cf10c505bd27
A Toy Soldier <azeezadewale98@gmail.com>
2018-07-05 02:20:10 +0100

    minor compatibility for web design - cgi

--- simple/sources/simple_vm.c
+++ simple/sources/simple_vm.c
@@ -1180,7 +1180,7 @@ SIMPLE_API void simple_vm_cgi_showerrormessage ( VM *vm,const char *cStr )
 	int x,lBlockCall,is_last_block  ;
 	List *list  ;
 	const char *cFile  ;
-	printf("find result of call2  %i \n",simple_list_findstring(vm->sState->files_stack,vm->file_name,0));
+	printf("find result of call2  %i <br />",simple_list_findstring(vm->sState->files_stack,vm->file_name,0));
 	/* Print the Error Message */
 	printf("<table border='1' cellspacing='0' cellpadding='2'>\n");
 	printf("<tr><th align='left' style='background:rgb(190,25,49);' colspan='5'><span style='color:white;'> Line %d : %s in %s</br></span></th></tr>\n",vm->nLineNumber,cStr,simple_list_getstring(vm->sState->files_list,1));
