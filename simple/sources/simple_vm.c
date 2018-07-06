ebd9c506b3355bff6473dd490694f93c50e1368b
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-06 15:43:36 +0100

    reformat and build more efficient debugging module

--- simple/sources/simple_vm.c
+++ simple/sources/simple_vm.c
@@ -1158,7 +1158,7 @@ SIMPLE_API void simple_vm_showerrormessage ( VM *vm,const char *cStr )
 			lBlockCall = 1 ;
 		}
 		else {
-			printf( "In %s ",file_real_name(simple_list_getstring(list,SIMPLE_BLOCKCL_NAME)) ) ;
+			printf( "\tin %s ",file_real_name(simple_list_getstring(list,SIMPLE_BLOCKCL_NAME)) ) ;
 		}
 	}
 	if ( lBlockCall ) {
