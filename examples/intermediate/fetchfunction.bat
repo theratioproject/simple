460107e68e2a9082d6cbf0df5a63a99564b27cc9
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-07 15:48:38 +0100

    stack_trace: reformat and build more efficient debugging module

--- examples/intermediate/fetchfunction.bat
+++ examples/intermediate/fetchfunction.bat
@@ -1,14 +1,13 @@
 
-call:generatelibfromdll
+call:visualstudio
+call:generatelibfromdll %1
 exit /b 0
 
 :generatelibfromdll
 	if !THERE_IS_VS!=="true" (
-		REM call:header configure "configure build %VERSION%"
-		REM call:locatevisualstudio !BUILD_ARC!
-		REM dumpbin /EXPORTS %1 > %1.txt
-		REM findstr %2 %1.txt > %1.2.txt
-		REM findstr %3 %1.2.txt > %1.3.txt
-		SET THERE_IS_VS="false"
+		call:locatevisualstudio !BUILD_ARC!
+		dumpbin /EXPORTS %1 > %1.txt
+		findstr %2 %1.txt > %1.2.txt
+		findstr %3 %1.2.txt > %1.3.txt
 	)
 	exit /b 0
