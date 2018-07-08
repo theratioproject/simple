efcacdbb7838c80a0427a179a89e4e8b26c4ed21
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-08 11:51:55 +0100

    stack_trace: reformat and build more efficient debugging module

--- simple/includes/simple_misc.h
+++ simple/includes/simple_misc.h
@@ -18,6 +18,16 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/*
+	Support for GCC Compiler with no TRUE and FALSE predefine
+	Starting with Haiku OS
+*/
+#ifdef TRUE && FALSE
+#else
+#define TRUE 1
+#define FALSE 0
+#endif
     
 typedef struct GCData {
 	int nReferenceCount  ;
