5885100734ac860c99397516b1a6a714b498fd0d
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-17 23:28:14 +0100

    modular:make modular install module and also fetch dependencies

--- build/Windows-Build.bat
+++ build/Windows-Build.bat
@@ -1315,7 +1315,7 @@ REM ENVIRONMENT PROGRAM BUILD ERROR
 	echo 	-c --configure	configure your system for simple-lang successfull build
 	echo 	-i --install	install simple-lang on your system
 	echo 	-b --build-dir	manually enter folder to install simple-lang 
-	echo 	-u --uninstall	uninstall simple-lang from your system
+	echo 	-u --uninstall	remove simple-lang installtion from your system
 	echo 	-d --debug	create a distributable version in ..\..\ source directory
 	echo 	x86 --32-bit	build 32 bit version of simple-lang
 	echo 	x64 --64-bit	build 64 bit version of simple-lang
