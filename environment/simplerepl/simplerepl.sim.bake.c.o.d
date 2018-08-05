83aab877080d1c4790e52b41ce83661fc917ee44
Azeez Adewale <azeezadewale98@gmail.com>
2018-08-05 09:35:44 +0100

    resolve lib crypto error in linux build

--- environment/simplerepl/simplerepl.sim.bake.c.o.d
+++ environment/simplerepl/simplerepl.sim.bake.c.o.d
@@ -1,44 +1,44 @@
 ..\environment\simplerepl\simplerepl.sim.bake.c.o: \
  ..\environment\simplerepl\simplerepl.sim.bake.c \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_misc.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_string.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_item.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_list.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_hashtable.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_state.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_scanner.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_parser.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_codegen.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_vm.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_vmgc.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_api.h \
- C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_vmdll.h
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_misc.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_string.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_item.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_list.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_hashtable.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_state.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_scanner.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_parser.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_codegen.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_vm.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_vmgc.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_api.h \
+ c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_vmdll.h
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_misc.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_misc.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_string.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_string.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_item.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_item.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_list.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_list.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_hashtable.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_hashtable.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_state.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_state.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_scanner.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_scanner.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_parser.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_parser.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_codegen.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_codegen.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_vm.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_vm.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_vmgc.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_vmgc.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_api.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_api.h:
 
-C:\Users\thecarisma\Documents\SIMPLE_GITHUB\s0.3.36-debug\bin\../bin/../includes/simple_vmdll.h:
+c:\users\thecarisma\documents\simple_github\s0.3.36-debug\includes\simple_vmdll.h:
