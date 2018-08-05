c9de3b7f5974364ec2535d437898f43d9653dd62
Azeez Adewale <azeezadewale98@gmail.com>
2018-08-05 07:36:20 +0100

    resolve lib crypto error in linux build

--- simple/makefiles/Makefile-Linux.mk
+++ simple/makefiles/Makefile-Linux.mk
@@ -189,9 +189,6 @@ install: $(CND_DISTDIR)/simple
 	install $(CND_DISTDIR)/simple $(DESTDIR)$(PREFIX)/bin/
 	install $(CND_DISTDIR)/simple.$(CND_DLIB_EXT) $(DESTDIR)$(PREFIX)/lib/
 	install $(INCLUDES_DIR)/simple* $(DESTDIR)$(PREFIX)/include/simple/
-	link $(DESTDIR)$(PREFIX)/lib/simple.$(CND_DLIB_EXT) $(DESTDIR)$(PREFIX)/lib/libsimple.$(CND_DLIB_EXT) 
-	link $(DESTDIR)$(PREFIX)/lib/simple.$(CND_DLIB_EXT) $(DESTDIR)$(PREFIX)/lib/x86_64-linux-gnu/libsimple.$(CND_DLIB_EXT)
-	link $(DESTDIR)$(PREFIX)/lib/simple.$(CND_DLIB_EXT) $(DESTDIR)$(PREFIX)/lib/x86_64-linux-gnu/libsimple.$(VER).$(CND_DLIB_EXT)
 
 .PHONY: uninstall
 uninstall:
@@ -200,9 +197,6 @@ uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/lib/x86_64-linux-gnu/libsimple.$(CND_DLIB_EXT)
 	rm -f $(DESTDIR)$(PREFIX)/lib/x86_64-linux-gnu/libsimple.$(VER).$(CND_DLIB_EXT)
 	rm -R -f $(DESTDIR)$(PREFIX)/include/simple/
-	unlink $(DESTDIR)$(PREFIX)/lib/libsimple.$(CND_DLIB_EXT) 
-	unlink $(DESTDIR)$(PREFIX)/lib/x86_64-linux-gnu/libsimple.$(CND_DLIB_EXT)
-	unlink $(DESTDIR)$(PREFIX)/lib/x86_64-linux-gnu/libsimple.$(VER).$(CND_DLIB_EXT)
 
 #This Makefile-Windows.mk was written in adaptation to the standard
 #method of writing makefiles
