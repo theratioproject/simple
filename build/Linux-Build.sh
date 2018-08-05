69e749f059c232584b9489a71f78b7260e0f63f3
Azeez Adewale <azeezadewale98@gmail.com>
2018-08-05 08:38:37 +0100

    resolve lib crypto error in linux build

--- build/Linux-Build.sh
+++ build/Linux-Build.sh
@@ -404,8 +404,8 @@ configure() {
 			*64* )
 				#sudo apt-get -y install libfltk1.3-dev:i386
 				sudo apt-get -y install xorg-dev:i386
-				#sudo apt-get -y install libxft-dev:i386
-				#sudo apt-get -y install libxcursor1:i386
+				sudo apt-get -y install libxft-dev:i386
+				sudo apt-get -y install libxcursor-dev:i386
 				sudo apt-get -y install libxinerama-dev:i386
 				sudo apt-get -y install libssl-dev:i386
 				sudo apt-get -y install libcurl4-openssl-dev:i386
