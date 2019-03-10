421e0fe47baa2eac1084c2b218eb67c19e193c82
codefii <ekeminyd@gmail.com>
2019-03-10 21:42:11 +0100

    add individual MAC build file in dynamic_modules

---
+++ modules/dynamic_modules/networker/Makefile-Mac.mk
@@ -0,0 +1,70 @@
+#Build networker dynamic module for LINUX
+#
+#There is no tool or dependency needed on a typical linux
+#installation. To build the simple.so and simple change your
+#directory to the modules/dynamic_modules/networker/ folder 
+#
+#user $ cd modules/dynamic_modules/networker/
+#
+#This is to ensure That the output generated are in 
+#appropriate `dist` folder in modules folder. 
+#Execute the below command in your command prompt of bash
+#
+#simple/src/makefiles/networker/ $ make -f Makefile-Linux.mk
+
+# Flags
+CFLAGS= -c -fpic -g -w -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/bin
+LFlAGS= -lm -ldl
+LDFLAGS= "-Wl,-rpath,./ ../../bin/ $(DESTDIR)$(PREFIX)/lib/ ~/lib/,--no-as-needed"
+
+# Macros
+CND_PLATFORM=
+CND_DLIB_EXT=dylib
+CND_BUILDDIR=build
+CND_DISTDIR =../dist
+
+# Object Directory
+OBJECTDIR=$(CND_DISTDIR)/$(CND_BUILDDIR)/$(CND_PLATFORM)
+
+# Simple Object Directory
+SIMPLE_OBJECTDIR=../../../simple/dist/$(CND_BUILDDIR)/$(CND_PLATFORM)
+
+# Object Files
+OBJECTFILES= \
+	$(OBJECTDIR)/networker.o
+
+# Simple Object Files
+SIMPLE_OBJECTFILES = \
+	$(SIMPLE_OBJECTDIR)/simple_api.o 
+	
+# Link Libraries and Options
+LDLIBSOPTIONS=../../../simple/dist/libsimple.${CND_DLIB_EXT} -lcurl -lcrypto -lssl
+
+${CND_DISTDIR}/${CND_PLATFORM}/libnetworker.${CND_DLIB_EXT}: $(OBJECTFILES)
+	$(CC) -shared $(ARC_FLAG) -o $(CND_DISTDIR)/$(CND_PLATFORM)/libnetworker.$(CND_DLIB_EXT) $(OBJECTFILES) $(LDLIBSOPTIONS)
+
+$(OBJECTDIR)/networker.o: networker.c
+	mkdir -p $(OBJECTDIR)
+	$(CC) $(ARC_FLAG) $(CFLAGS) networker.c
+	mv networker.o $(OBJECTDIR)
+
+clean:
+	@- $(RM) $(OBJECTDIR)/*.o
+	@- $(RM) $(program_OBJS)
+
+distclean: clean
+
+#This Makefile-Windows.mk was written in adaptation to the standard
+#method of writing makefiles
+#
+#If this make file does not work on your LINUX PC feel free to 
+#write your own Makefile and send us a pull request at 
+#https://github.com/simple-lang/simple . Your make file must follow 
+#the below format for request to be accepted
+#
+#File-Name : Makefile-Linux-{distro}.mk
+#
+#e.g Makefile-Linux-mint-cinnamon.mk
+#
+#looking forward to your contribution
+#Thank You
---
+++ modules/dynamic_modules/simple_sqlite/Makefile-Mac.mk
@@ -0,0 +1,76 @@
+#Build simple_sqlite dynamic module for LINUX
+#
+#There is no tool or dependency needed on a typical linux
+#installation. To build the simple.so and simple change your
+#directory to the modules/dynamic_modules/simple_sqlite/ folder 
+#
+#user $ cd modules/dynamic_modules/simple_sqlite/
+#
+#This is to ensure That the output generated are in 
+#appropriate `dist` folder in modules folder. 
+#Execute the below command in your command prompt of bash
+#
+#simple/src/makefiles/simple_sqlite/ $ make -f Makefile-Linux.mk
+
+# Flags
+CFLAGS= -c -fpic -g -w
+LFlAGS= -lm -ldl
+LDFLAGS= "-Wl,-rpath,./ ../../bin/ $(DESTDIR)$(PREFIX)/lib/ ~/lib/,--no-as-needed"
+
+# Macros
+CND_PLATFORM=
+CND_DLIB_EXT=dylib
+CND_BUILDDIR=build
+CND_DISTDIR =../dist
+
+# Object Directory
+OBJECTDIR=$(CND_DISTDIR)/$(CND_BUILDDIR)/$(CND_PLATFORM)
+
+# Simple Object Directory
+SIMPLE_OBJECTDIR=../../../simple/dist/$(CND_BUILDDIR)/$(CND_PLATFORM)
+
+# Object Files
+OBJECTFILES= \
+	$(OBJECTDIR)/simple_sqlite.o \
+	$(OBJECTDIR)/sqlite3.o
+
+# Simple Object Files
+SIMPLE_OBJECTFILES = \
+	$(SIMPLE_OBJECTDIR)/simple_api.o 
+	
+# Link Libraries and Options
+LDLIBSOPTIONS=../../../simple/dist/libsimple.${CND_DLIB_EXT}
+
+${CND_DISTDIR}/${CND_PLATFORM}/libsimple_sqlite.${CND_DLIB_EXT}: $(OBJECTFILES)
+	$(CC) -shared $(ARC_FLAG) -o $(CND_DISTDIR)/$(CND_PLATFORM)/libsimple_sqlite.$(CND_DLIB_EXT) $(OBJECTFILES) $(LDLIBSOPTIONS)
+
+$(OBJECTDIR)/simple_sqlite.o: simple_sqlite.c
+	mkdir -p $(OBJECTDIR)
+	$(CC) $(ARC_FLAG) $(CFLAGS) simple_sqlite.c
+	mv simple_sqlite.o $(OBJECTDIR)
+
+$(OBJECTDIR)/sqlite3.o: sqlite3.c
+	mkdir -p $(OBJECTDIR)
+	$(CC) $(ARC_FLAG) $(CFLAGS) sqlite3.c
+	mv sqlite3.o $(OBJECTDIR)
+
+clean:
+	@- $(RM) $(OBJECTDIR)/*.o
+	@- $(RM) $(program_OBJS)
+
+distclean: clean
+
+#This Makefile-Windows.mk was written in adaptation to the standard
+#method of writing makefiles
+#
+#If this make file does not work on your LINUX PC feel free to 
+#write your own Makefile and send us a pull request at 
+#https://github.com/simple-lang/simple . Your make file must follow 
+#the below format for request to be accepted
+#
+#File-Name : Makefile-Linux-{distro}.mk
+#
+#e.g Makefile-Linux-mint-cinnamon.mk
+#
+#looking forward to your contribution
+#Thank You
